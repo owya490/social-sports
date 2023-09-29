20c1e954c4b16f42effc1f31e7db7062387eef12
unknown <aidanyschee@gmail.com>
2023-09-29 14:23:54 +1000

    Aidans first commit

--- frontend/app/event/[id]/page.tsx
+++ frontend/app/event/[id]/page.tsx
@@ -8,15 +8,12 @@ export default function EventDetails({ params }: any) {
                 <div className="grid grid-rows-2 gap-10 grid-cols-9">
                     <div className="row-start-1 col-start-2 col-span-4 border border-1">
                         <img 
-                        src="https://tecdn.b-cdn.net/img/new/standard/city/041.jpg"
-                        className="mx-auto object-cover h-1/2 w-full"
+                        src="https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200"
+                        className="object-cover w-full h-full"
                         alt="..." />
                     </div>
                     <div className="row-start-2 col-start-2 col-span-4 border border-1">
-                        <img
-                        src="https://tecdn.b-cdn.net/img/new/standard/city/041.jpg"
-                        className="mx-auto object-cover h-full w-full"
-                        alt="..." />
+                        
                     </div>
                     <div className="row-start-1 row-span-2 col-start-7 col-span-2 border border-1">
                         reggie star this is ur part
