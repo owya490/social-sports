5561134507f4c2563534ae25ec6a47ff9c826806
unknown <aidanyschee@gmail.com>
2023-10-03 14:55:36 +1100

    config-file

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,45 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  theme: {
+    screens: {
+      xs: "100px",
+      ...defaultTheme.screens,
+    },
+  },
+  plugins: [],
+};
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "1600px",
+        "4xl": "2600px",
+      },
+    },
+  },
+  plugins: [],
+};
