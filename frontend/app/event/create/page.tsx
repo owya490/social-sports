1d8dd4e8363b2c9d0d22df684176a1b9a0eb2a0e
Richard Peng <richardpeng914@gmail.com>
2023-10-19 23:13:15 +1100

    for pc

--- frontend/app/event/create/page.tsx
+++ frontend/app/event/create/page.tsx
@@ -1,20 +1,11 @@
-"use client"
-import CreateEventTimeline from "@/components/events/create/CreateEventTimeline";
-import { useState } from "react";
+"use client";
 
-export default function CreateEvent() { 
-    const [hidden1, setHidden1] = useState(true)
-  return (
-    <div className="pt-20">
-      <p>hello</p>
-    {hidden1 && 
-      <CreateEventTimeline hidden={hidden1}/>
-    }
-   
-      <button onClick={()=> {
-        console.log (hidden1)
-        setHidden1(!hidden1)
-      }}>click me</button>
-    </div>
-  );
+import { CreateEventTimeline } from "@/components/events/create/CreateEventTimeline";
+
+export default function CreateEvent() {
+    return (
+        <div>
+            <CreateEventTimeline />
+        </div>
+    );
 }
--- frontend/components/events/create/CreateEventTimeline.tsx
+++ frontend/components/events/create/CreateEventTimeline.tsx
@@ -1,9 +1,38 @@
-export default function CreateEventTimeline(hidden:{hidden:boolean}) {
-    
+import React from "react";
+
+export function CreateEventTimeline() {
     return (
-      <div>
-        <p>aidan</p>
-      </div>
+        <div className="mt-20">
+            <ol className="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
+                <li className="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+                    <span className="flex items-center justify-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-blue-600 dark:bg-blue-500 text-white">
+                        1
+                    </span>
+                    Personal{" "}
+                    <span className="hidden sm:inline-flex sm:ml-2">Info</span>
+                </li>
+                <li className="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+                    <span className="flex items-center justify-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-blue-600 dark:bg-blue-500 text-white">
+                        2
+                    </span>
+                    Account{" "}
+                    <span className="hidden sm:inline-flex sm:ml-2">Info</span>
+                </li>
+                <li className="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+                    <span className="flex items-center justify-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-blue-600 dark:bg-blue-500 text-white">
+                        3
+                    </span>
+                    Account{" "}
+                    <span className="hidden sm:inline-flex sm:ml-2">Info</span>
+                </li>
+                <li className="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
+                    <span className="flex items-center justify-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-blue-600 dark:bg-blue-500 text-white">
+                        4
+                    </span>
+                    Account{" "}
+                    <span className="hidden sm:inline-flex sm:ml-2">Info</span>
+                </li>
+            </ol>
+        </div>
     );
-  }
-  
+}
