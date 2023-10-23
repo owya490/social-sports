4e45aff5ebed48af611ae1865d75a1252e7057c7
Brian Wang <brian.wang@student.unsw.edu.au>
2023-10-23 18:25:00 +1100

    initial commit

--- functions/main.py
+++ functions/main.py
@@ -2,12 +2,30 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
-from firebase_admin import initialize_app
+from firebase_functions import firestore_fn, https_fn
+from firebase_admin import initialize_app, firestore
+import google.cloud.firestore
 
-# initialize_app()
-#
-#
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+initialize_app()
+
+@https_fn.on_request()
+def addmessage(req: https_fn.Request) -> https_fn.Response:
+    """Take the text parameter passed to this HTTP endpoint and insert it into
+    a new document in the messages collection."""
+
+    # Grab the search_string parameter.
+    original = req.args.get("search_string")
+    if original is None:
+        return https_fn.Response("No text parameter provided", status=400)
+
+    firestore_client: google.cloud.firestore.Client = firestore.client()
+
+    # Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    _, doc_ref = firestore_client.collection("messages").add(
+        {"original": original}
+    )
+
+    # Send back a message that we've successfully written the message
+    return https_fn.Response(f"Message with ID {doc_ref.id} added.")
+
+
