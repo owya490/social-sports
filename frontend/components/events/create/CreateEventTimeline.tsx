8fc8ddde8832081607e9169fb489490a43a6ee13
richardpeng914@gmail.com <richardpeng914@gmail.com>
2023-10-23 16:27:01 +1100

    some updates (not that much)

--- frontend/components/events/create/CreateEventTimeline.tsx
+++ frontend/components/events/create/CreateEventTimeline.tsx
@@ -2,35 +2,34 @@ import React from "react";
 
 export function CreateEventTimeline() {
     return (
-        <div className="mt-20">
+        <div className="mt-20 relative">
             <ol className="flex items-center w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
-                <li className="flex md:w-full items-center text-blue-600 dark:text-blue-500 sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-                    <span className="flex items-center justify-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-blue-600 dark:bg-blue-500 text-white">
+                <li className="flex md:w-full items-center flex-col relative">
+                    <span className="flex items-center justify-center border border-black after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-blue-600 dark:bg-blue-500 text-black">
                         1
                     </span>
-                    Personal{" "}
-                    <span className="hidden sm:inline-flex sm:ml-2">Info</span>
+                    <span className="text-black">Basic Information</span>
+                    
                 </li>
-                <li className="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-                    <span className="flex items-center justify-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-blue-600 dark:bg-blue-500 text-white">
+                <li className="flex md:w-full items-center flex-col relative">
+                    <span className="flex items-center justify-center border border-black after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-gray-300 dark:bg-gray-400 text-black">
                         2
                     </span>
-                    Account{" "}
-                    <span className="hidden sm:inline-flex sm:ml-2">Info</span>
+                    <span className="text-black">Relevant Tags</span>
+                    
                 </li>
-                <li className="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-                    <span className="flex items-center justify-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-blue-600 dark:bg-blue-500 text-white">
+                <li className="flex md:w-full items-center flex-col relative">
+                    <span className="flex items-center justify-center border border-black after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-gray-300 dark:bg-gray-400 text-black">
                         3
                     </span>
-                    Account{" "}
-                    <span className="hidden sm:inline-flex sm:ml-2">Info</span>
+                    <span className="text-black">Description and Image</span>
+                    
                 </li>
-                <li className="flex md:w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
-                    <span className="flex items-center justify-center after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-blue-600 dark:bg-blue-500 text-white">
+                <li className="flex md:w-full items-center flex-col">
+                    <span className="flex items-center justify-center border border-black after:content-['/'] sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500 rounded-full w-8 h-8 bg-gray-300 dark:bg-gray-400 text-black">
                         4
                     </span>
-                    Account{" "}
-                    <span className="hidden sm:inline-flex sm:ml-2">Info</span>
+                    <span className="text-black">Create the Event</span>
                 </li>
             </ol>
         </div>
