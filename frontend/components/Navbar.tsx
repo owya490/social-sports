c8c052dca791773a00f8e633b7e27aed9f1b4a7d
unknown <aidanyschee@gmail.com>
2023-10-26 14:24:57 +1100

    profile pic

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,82 +1,48 @@
 "use client";
 
 import Image from "next/image";
-import DP from "./../public/images/Ashley & Owen.png";
+import { useState } from "react";
 import Logo from "./../public/images/Logo.png";
+import ProfilePic from "./ProfilePic";
 import SearchBar from "./SearchBar";
-import React, { useState } from 'react';
-
-// import { Roboto_Condensed } from "next/font/google";
-
-// const roboto_condensed = Roboto_Condensed({
-//     weight: "300",
-//     subsets: ["latin"],
-//     display: "swap",
-//     variable: "--font-roboto-condensed",
-// });
 
 export default function Navbar() {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const toggleDropdown = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
-    };
+  };
 
-    return (
-        <div className="bg-white drop-shadow-lg fixed top-0 w-screen z-50">
-            <div className="flex items-center py-2 px-10">
-                <Image
-                    src={Logo}
-                    alt="Logo"
-                    width={50}
-                    height={50}
-                    className="w-12 mx-1"
-                />
-                <h1 className="font-robotocondensed text-3xl font-extrabold mr-20">
-                    SOCIAL SPORTS
-                </h1>
-
-                <SearchBar />
-                <div className="flex ml-auto items-center">
-                    <button
-                        className=" border border-black px-3 py-2 rounded-full mx-5"
-                        onChange={(e) => {
-                            window.open("https://www.google.com", "_self");
-                        }}
-                    >
-                        Create Event
-                    </button>
-                    <button id="dropdownDividerButton" onClick={toggleDropdown} className=" border border-black rounded-full w-10 h-10">
-                        <Image
-                            src={DP}
-                            alt="DP"
-                            width={50}
-                            height={50}
-                            className="rounded-full w-10 h-10"
-                        />
-                    </button>
-                    {isDropdownOpen && 
-                    <div id="dropdownDivider" className="z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                        <li>
-                            <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                        </li>
-                        </ul>
-                    </div>
-                    }
-                </div>
-
-                {/* <div className="rounded-full w-10 h-10 bg-red-200 ml-auto"></div> */}
-            </div>
-            <div className="h-[1px] bg-black"></div>
+  return (
+    <div className="bg-white drop-shadow-lg fixed top-0 w-screen z-50">
+      <div className="flex items-center py-2 px-10">
+        <div className="flex items-center">
+          <Image
+            src={Logo}
+            alt="Logo"
+            width={50}
+            height={50}
+            className="w-12 mx-1"
+          />
+          <h1 className="font-robotocondensed text-3xl font-extrabold mr-10">
+            SOCIAL SPORTS
+          </h1>
         </div>
-    );
+        <SearchBar />
+        <div className="ml-auto flex items-center">
+          <button
+            className="border border-black px-3 py-2 rounded-full mx-5 max-h-[40px]"
+            onClick={(e) => {
+              window.open("https://www.google.com", "_self");
+            }}
+          >
+            Create Event
+          </button>
+          <div className="mt-1">
+            <ProfilePic />
+          </div>
+        </div>
+      </div>
+      <div className="h-[1px] bg-black"></div>
+    </div>
+  );
 }
---
+++ frontend/components/ProfilePic.tsx
@@ -0,0 +1,134 @@
+import { Menu, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { Fragment } from "react";
+import DP from "./../public/images/Ashley & Owen.png";
+
+const loggedin = false;
+
+export default function Example() {
+  return (
+    <div>
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex w-full justify-center">
+            <Image
+              src={DP}
+              alt="DP"
+              width={50}
+              height={50}
+              className="rounded-full w-10 h-10 border border-black"
+            />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 w-52 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-1 py-1">
+              {!loggedin && (
+                <>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="/login"
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        Log In
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="/signup"
+                        className={`${
+                          active ? "bg-violet-500 text-white" : "text-gray-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        Sign Up
+                      </a>
+                    )}
+                  </Menu.Item>
+                </>
+              )}
+            </div>
+            {loggedin && (
+              <div className="px-1 py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/profile"
+                      className={`${
+                        active ? "bg-violet-500 text-white" : "text-gray-900"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Profile
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/settings"
+                      className={`${
+                        active ? "bg-violet-500 text-white" : "text-gray-900"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Settings
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="/logout"
+                      className={`${
+                        active ? "bg-violet-500 text-white" : "text-gray-900"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      Log Out
+                    </a>
+                  )}
+                </Menu.Item>
+              </div>
+            )}
+            <div className="px-1 py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="/help"
+                    className={`${
+                      active ? "bg-violet-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    Help Centre
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="/help"
+                    className={`${
+                      active ? "bg-violet-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    Suggestions
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+}
