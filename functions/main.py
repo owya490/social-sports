0c2fe22c18ffab7e212ad5e5caca6eab3c9ef9a2
Brian Wang <brian.wang@student.unsw.edu.au>
2023-10-27 23:22:42 +1100

    commented out stub function in main.py

--- functions/main.py
+++ functions/main.py
@@ -8,24 +8,24 @@ import google.cloud.firestore
 
 initialize_app()
 
-@https_fn.on_request()
-def addmessage(req: https_fn.Request) -> https_fn.Response:
-    """Take the text parameter passed to this HTTP endpoint and insert it into
-    a new document in the messages collection."""
+# @https_fn.on_request()
+# def addmessage(req: https_fn.Request) -> https_fn.Response:
+#     """Take the text parameter passed to this HTTP endpoint and insert it into
+#     a new document in the messages collection."""
 
-    # Grab the search_string parameter.
-    original = req.args.get("search_string")
-    if original is None:
-        return https_fn.Response("No text parameter provided", status=400)
+#     # Grab the search_string parameter.
+#     original = req.args.get("search_string")
+#     if original is None:
+#         return https_fn.Response("No text parameter provided", status=400)
 
-    firestore_client: google.cloud.firestore.Client = firestore.client()
+#     firestore_client: google.cloud.firestore.Client = firestore.client()
 
-    # Push the new message into Cloud Firestore using the Firebase Admin SDK.
-    _, doc_ref = firestore_client.collection("messages").add(
-        {"original": original}
-    )
+#     # Push the new message into Cloud Firestore using the Firebase Admin SDK.
+#     _, doc_ref = firestore_client.collection("messages").add(
+#         {"original": original}
+#     )
 
-    # Send back a message that we've successfully written the message
-    return https_fn.Response(f"Message with ID {doc_ref.id} added.")
+#     # Send back a message that we've successfully written the message
+#     return https_fn.Response(f"Message with ID {doc_ref.id} added.")
 
 
