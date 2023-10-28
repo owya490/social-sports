0a12bf97762e8d8bc220155e7abb3a6e0a07c5dc
Owen <owen.yang@sowaustralia.com>
2023-10-28 16:01:34 +1100

    suggestions form

---
+++ frontend/app/suggestions/page.tsx
@@ -0,0 +1,149 @@
+import Logo from "@/components/navbar/Logo";
+
+export default function Suggestions() {
+  return (
+    <div className="pt-20 w-screen flex justify-center bg-white">
+      <div className="max-w-lg space-y-5">
+        <div className="flex justify-center">
+          <Logo />
+        </div>
+        <div
+          className="px-7 py-3 leading-normal bg-blue-100 rounded-lg text-center"
+          role="alert"
+        >
+          <span className="flex items-center">
+            <svg
+              version="1.1"
+              id="Layer_1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+              viewBox="0 0 512 512"
+              xmlSpace="preserve"
+              fill="#000000"
+              className="w-10 px-2"
+            >
+              <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+              <g
+                id="SVGRepo_tracerCarrier"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <g id="SVGRepo_iconCarrier">
+                {" "}
+                <path
+                  style={{ fill: "#576D7E" }}
+                  d="M278.43,512H233.57c-6.168,0-11.8-3.495-14.547-9.021l-11.784-23.487h97.524l-11.784,23.487 C290.231,508.505,284.599,512,278.43,512z"
+                />{" "}
+                <path
+                  style={{ fill: "#C9D5DD" }}
+                  d="M190.984,365.714h130.032c4.486,0,8.127,3.641,8.127,8.127v89.397c0,8.98-7.274,16.254-16.254,16.254 H199.111c-8.98,0-16.254-7.274-16.254-16.254v-89.397C182.857,369.355,186.498,365.714,190.984,365.714z"
+                />{" "}
+                <path
+                  style={{ fill: "#FCD063" }}
+                  d="M256,0c-85.252,0-154.413,60.952-154.413,146.286c0.431,47.738,17.701,93.785,48.762,130.032 c15.523,19.017,40.635,57.458,40.635,89.397h130.032c0-31.939,25.112-70.38,40.635-89.397 c31.061-36.246,48.331-82.294,48.762-130.032C410.413,60.952,341.252,0,256,0z"
+                />{" "}
+                <path
+                  style={{ fill: "#F6B545" }}
+                  d="M321.016,365.714H190.984c0-8.98,7.274-16.254,16.254-16.254h99.068 c7.152-38.116,36.571-75.825,42.667-83.383c25.21-29.249,40.741-65.593,44.455-104.025c0.683-8.883,8.078-15.75,16.985-15.766l0,0 c-0.203,47.779-17.497,93.899-48.762,130.032c-6.014,7.396-36.328,46.243-40.635,82.164V365.714z"
+                />{" "}
+                <g>
+                  {" "}
+                  <path
+                    style={{ fill: "#FFE27A" }}
+                    d="M148.155,105.651c-4.486,0.065-8.176-3.527-8.241-8.013c-0.024-1.463,0.358-2.909,1.089-4.177 C163.58,50.916,207.831,24.332,256,24.381c4.486,0,8.127,3.641,8.127,8.127s-3.641,8.127-8.127,8.127 c-42.285,0.033-81.059,23.503-100.693,60.952C153.836,104.139,151.097,105.691,148.155,105.651z"
+                  />{" "}
+                  <path
+                    style={{ fill: "#FFE27A" }}
+                    d="M134.095,162.54c-4.486,0-8.127-3.641-8.127-8.127c0-8.737,0.87-17.449,2.601-26.006 c0.894-4.486,5.266-7.396,9.752-6.502c4.486,0.894,7.396,5.266,6.502,9.752c-1.625,7.477-2.495,15.1-2.601,22.756 C142.222,158.899,138.581,162.54,134.095,162.54z"
+                  />{" "}
+                </g>{" "}
+                <path
+                  style={{ fill: "#ECF0F9" }}
+                  d="M304.762,146.286c-17.953,0-32.508,14.555-32.508,32.508v16.254h-24.381v-16.254 c0-17.952-14.555-32.508-32.508-32.508s-32.508,14.555-32.508,32.508s14.555,32.508,32.508,32.508h16.254V349.46h16.254V211.302 h24.381V349.46h16.254V211.302h16.254c17.952,0,32.508-14.555,32.508-32.508S322.714,146.286,304.762,146.286z M215.365,195.048 c-8.98,0-16.254-7.274-16.254-16.254s7.274-16.254,16.254-16.254s16.254,7.274,16.254,16.254v16.254H215.365z M304.762,195.048 h-16.254v-16.254c0-8.98,7.274-16.254,16.254-16.254s16.254,7.274,16.254,16.254S313.742,195.048,304.762,195.048z"
+                />{" "}
+                <g>
+                  {" "}
+                  <rect
+                    x="231.619"
+                    y="349.46"
+                    style={{ fill: "#C9D5E3" }}
+                    width="16.254"
+                    height="16.254"
+                  />{" "}
+                  <rect
+                    x="272.254"
+                    y="349.46"
+                    style={{ fill: "#C9D5E3" }}
+                    width="16.254"
+                    height="16.254"
+                  />{" "}
+                </g>{" "}
+                <g>
+                  {" "}
+                  <path
+                    style={{ fill: "#B2C4D0" }}
+                    d="M296.635,463.238H182.857l0,0v-16.254l0,0h97.524C289.361,446.984,296.635,454.258,296.635,463.238 L296.635,463.238L296.635,463.238z"
+                  />{" "}
+                  <path
+                    style={{ fill: "#B2C4D0" }}
+                    d="M231.619,381.968h97.524l0,0v16.254l0,0H215.365l0,0l0,0 C215.365,389.242,222.639,381.968,231.619,381.968z"
+                  />{" "}
+                  <rect
+                    x="182.857"
+                    y="414.476"
+                    style={{ fill: "#B2C4D0" }}
+                    width="146.286"
+                    height="16.254"
+                  />{" "}
+                </g>{" "}
+                <polygon
+                  style={{ fill: "#35495C" }}
+                  points="296.635,495.746 304.762,479.492 207.238,479.492 215.365,495.746 "
+                />{" "}
+              </g>
+            </svg>
+
+            <h3 className="font-bold text-xl">
+              We Value your Suggestions and Feedback.
+            </h3>
+          </span>
+
+          <div className="flex justify-center">
+            <p className="w-3/4">
+              We appreciate you taking the time to share your thoughts. Please
+              input your Feedback/ Suggestion in the box below.
+            </p>
+          </div>
+        </div>
+        <div className="space-y-5">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="col-span-1 w-full">
+              <label className="block text-gray-700 text-sm font-bold mb-1">
+                First Name
+              </label>
+              <input className="border border-black px-2 py-1 rounded-lg w-full"></input>
+            </div>
+            <div className="col-span-1 w-full">
+              <label className="block text-gray-700 text-sm font-bold mb-1">
+                Last Name
+              </label>
+              <input className="border border-black px-2 py-1 rounded-lg w-full"></input>
+            </div>
+          </div>
+          <div className="w-full">
+            <label className="block text-gray-700 text-sm font-bold mb-1">
+              Email
+            </label>
+            <input className="border border-black px-2 py-1 rounded-lg w-full"></input>
+          </div>
+          <div className="w-full">
+            <label className="block text-gray-700 text-sm font-bold mb-1">
+              Suggestion/ Feedback
+            </label>
+            <textarea className="border border-black px-2 py-1 rounded-lg w-full h-32"></textarea>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
