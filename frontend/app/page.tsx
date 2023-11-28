8ed3546f9ddce9a555340ca34fb7fff846d0a85a
Owen <owen.yang@sowaustralia.com>
2023-11-28 23:32:20 +1100

    remove use client

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,3 @@
-"use client";
-import FilterDialog from "@/components/FilterDialog";
-
 export default function Home() {
-  return (
-    <main>
-      <div className="mt-20">
-        <FilterDialog />
-      </div>
-    </main>
-  );
+  return <main></main>;
 }
