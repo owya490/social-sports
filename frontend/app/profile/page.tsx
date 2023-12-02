15115b4a2800aedd2fc56443c7bee33f498625fe
unknown <aidanyschee@gmail.com>
2023-12-02 19:55:39 +1100

    idk

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -16,19 +16,17 @@ export default function Profile() {
   return (
     <div className="mt-[100px] mb-[5%] mx-10 grid grid-cols-1 lg:grid-cols-5 gap-x-[10vw] lg:gap-x-[2vw] gap-y-[5vw] lg:gap-y-[1vw] px-5 lg:px-10">
       <div
-        className="col-start-1 col-span-1 lg:row-span-2 border border-gray-500 max-w-[250px] min-w-[200px]"
+        className="col-start-1 col-span-1 lg:col-span-2 border border-gray-500 w-[250px]"
         style={{ borderRadius: "20px" }}
       >
-        <div className="grid justify-center mt-5 h-fit">
-          <div className="rounded-full border border-black overflow-hidden relative">
-            <Image
-              src={DP}
-              alt="DP"
-              width={150}
-              height={150}
-              className="w-150 h-150"
-            />
-          </div>
+        <div className="grid justify-center mt-5">
+          <Image
+            src={DP}
+            alt="DP"
+            width={160}
+            height={160}
+            className="relative inline-block h-150 w-150 rounded-full object-cover object-center border border-black overflow-hidden"
+          />
         </div>
         <div className="flex justify-center mt-5 text-2xl font-semibold">
           {firstName} {truncatedLastName} , {age}
@@ -36,7 +34,7 @@ export default function Profile() {
         <div className="flex justify-center mt-3 mb-5 text-lg ">{location}</div>
       </div>
       <div className="col-start-1 col-span-1 lg:col-start-3 lg:col-span-3 mt-5 lg:mt-0">
-        <div className="flex justify-center lg:justify-start text-3xl lg:text-4xl font-semibold">
+        <div className="flex justify-center lg:justify-start text-3xl lg:text-4xl font-semibold lg:my-3">
           Profile Details
         </div>
         <div className="flex justify-center lg:justify-start text-lg mt-5">
@@ -56,12 +54,12 @@ export default function Profile() {
             <li>
               <strong>Date of Birth:</strong> {birthday}
             </li>
-            <li>
+            {/* <li>
               <strong>Password:</strong> {password}
-            </li>
+            </li> */}
           </ul>
         </div>
-        <div className="flex justify-center lg:justify-start mt-5 mb-3">
+        <div className="flex justify-center lg:justify-start my-7">
           <button
             type="button"
             className=" text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-med px-6 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
