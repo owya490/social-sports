704d0796b4fa2045499f8ef223372ca944698887
unknown <aidanyschee@gmail.com>
2023-12-16 02:52:18 +1100

    lol10

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,8 +1,26 @@
 "use client";
 import { Dialog, Transition } from "@headlessui/react";
 import Image from "next/image";
-import React, { Fragment, useState } from "react";
+import { Fragment, useState } from "react";
 import DP from "./../../public/images/Ashley & Owen.png";
+import eye from "./../../public/images/Eye.png"
+
+const calculateAge = (birthday) => {
+  const [day, month, year] = birthday.split("-");
+  const birthDate = new Date(`${year}-${month}-${day}`);
+  const currentDate = new Date();
+  let age = currentDate.getFullYear() - birthDate.getFullYear();
+
+  if (
+    currentDate.getMonth() < birthDate.getMonth() ||
+    (currentDate.getMonth() === birthDate.getMonth() &&
+      currentDate.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+};
 
 const initialProfileData = {
   firstName: "Owen",
@@ -11,13 +29,14 @@ const initialProfileData = {
   phoneNumber: "0468368618",
   email: "owya490@gmail.com",
   birthday: "23-07-2002", // DD-MM-YYYY format
-  age: 21,
+  age: calculateAge("23-07-2002"), // Calculate initial age
   password: "danielinthesky",
 };
 
 const Profile = () => {
   const [editable, setEditable] = useState(false);
   const [editedData, setEditedData] = useState({ ...initialProfileData });
+
   const handleEditClick = () => {
     if (editable) {
       setEditedData({ ...initialProfileData });
@@ -73,10 +92,10 @@ const Profile = () => {
 
   const renderField = (label, name) => (
     <li key={label} className="mb-4">
-      <strong className="block text-sm font-medium text-gray-700">
+      <strong className="block text-md lg:text-lg 3xl:text-2xl font-medium text-gray-700">
         {label}:
       </strong>
-      <span className="mt-1 text-lg">
+      <span className="mt-1 text-lg lg:text-xl 3xl:text-3xl">
         {name === "password"
           ? "*".repeat(initialProfileData[name].length)
           : initialProfileData[name]}
@@ -113,11 +132,11 @@ const Profile = () => {
               <Dialog.Panel className="w-[90%] top-[6rem] absolute max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all z-50">
                 <Dialog.Title
                   as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900"
+                  className="text-xl lg:text-2xl 3xl:text-3xl font-medium leading-6 text-gray-900 mb-3"
                 >
                   Edit Profile
                 </Dialog.Title>
-                <div className="space-y-4">
+                <div className="space-y-4 text-md lg:text-lg 3xl:text-xl">
                   {renderEditableField("Given Name", "firstName")}
                   {renderEditableField("Surname", "lastName")}
                   {renderEditableField("Email", "email", "email")}
@@ -127,21 +146,20 @@ const Profile = () => {
                   {renderEditableField("Password", "password", "password")}
                 </div>
                 <div className="flex justify-end mt-4">
-                <button
-  type="button"
-  onClick={handleSaveClick}
-  className="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-med px-6 py-2 z-50"
->
-  Save
-</button>
-<button
-  type="button"
-  onClick={handleEditClick}
-  className="ml-4 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-red-300 font-medium rounded-full text-med px-6 py-2 z-50"
->
-  Cancel
-</button>
-
+                  <button
+                    type="button"
+                    onClick={handleSaveClick}
+                    className="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:ring-green-300 font-medium rounded-full text-med px-6 py-2 z-50"
+                  >
+                    Save
+                  </button>
+                  <button
+                    type="button"
+                    onClick={handleEditClick}
+                    className="ml-4 text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:ring-red-300 font-medium rounded-full text-med px-6 py-2 z-50"
+                  >
+                    Cancel
+                  </button>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
@@ -151,30 +169,44 @@ const Profile = () => {
     </Transition>
   );
 
+  const editedAge = calculateAge(editedData.birthday);
+
   return (
-    <div className="relative mt-[100px] mb-[5%] mx-10 grid grid-cols-1 lg:grid-cols-5 gap-x-[10vw] lg:gap-x-[2vw] gap-y-[5vw] lg:gap-y-[1vw] px-5 lg:px-10">
-      <div className="flex justify-center col-start-1 col-span-1 lg:col-span-2 lg:row-span-1">
-        <div className="mb-5 text-3xl "></div>
+    <div className="relative mt-[100px] mb-[5%] mx-10 grid grid-cols-1 lg:grid-cols-5 3xl:grid-cols-7 gap-x-[10vw] lg:gap-x-[2vw] gap-y-[2vw] lg:gap-y-[1vw] 3xl:gap-y-[0.5vw] px-5 lg:px-10">
+      <div className="flex justify-center col-start-1 col-span-1 lg:col-span-2 lg:row-span-1 lg:row-start-1 3xl:col-start-2">
         <div
-          className="border border-gray-500 w-[250px]"
+          className="mb-5 3xl:mb-9 text-3xl lg:text-4xl 3xl:text-5xl"
+          style={{ fontWeight: 500 }}
+        >
+          {initialProfileData.firstName}'s Profile
+        </div>
+      </div>
+      <div className="flex justify-center col-start-1 col-span-1 lg:col-span-2 lg:row-span-1 lg:row-start-2 3xl:col-start-2">
+        <div
+          className="border border-gray-500 w-[250px] 3xl:w-[400px]"
           style={{ borderRadius: "20px" }}
         >
           <div className="grid justify-center mt-5">
-            <div className="relative inline-block rounded-full object-cover object-center border border-black overflow-hidden">
-              <Image src={DP} alt="DP" width={0} height={0} className="rounded-full object-cover h-52 w-52"/>
+            <div className="relative inline-block rounded-full object-cover object-center border border-black overflow-hidden mx-2">
+              <Image
+                src={DP}
+                alt="DP"
+                width={0}
+                height={0}
+                className="rounded-full object-cover h-52 w-52 3xl:h-80 3xl:w-80"
+              />
             </div>
           </div>
-          <div className="flex justify-center mt-5 text-2xl font-semibold">
+          <div className="flex justify-center mt-5 3xl:mt-9 text-2xl 3xl:text-4xl font-semibold">
             {initialProfileData.firstName}{" "}
-            {initialProfileData.lastName?.slice(0, 1)} ,{" "}
-            {initialProfileData.age}
+            {initialProfileData.lastName?.slice(0, 1)}, {initialProfileData.age}
           </div>
-          <div className="flex justify-center mt-3 mb-5 text-lg ">
+          <div className="flex justify-center mt-3 3xl:mt-5 mb-5 text-lg 3xl:text-2xl">
             {initialProfileData.location}
           </div>
         </div>
       </div>
-      <div className="grid justify-center lg:justify-start col-start-1 col-span-1 lg:col-start-3 lg:col-span-3 lg:row-span-2 mt-5 lg:mt-0 lg:ml-10">
+      <div className="grid justify-center col-start-1 col-span-1 lg:col-start-3 lg:row-span-3 mt-5 lg:mt-0 lg:ml-10 3xl:col-start-4 3xl:text-xl">
         <ul>
           {renderField("Given Name", "firstName")}
           {renderField("Surname", "lastName")}
@@ -184,21 +216,45 @@ const Profile = () => {
           {renderField("Date of Birth", "birthday")}
           {renderField("Password", "password")}{" "}
         </ul>
-        <div className="flex justify-center lg:justify-start my-7">
+        <div className="flex justify-center lg:justify-start my-2 3xl:my-4">
           {renderModalContent()}
           {!editable && (
             <button
-            type="button"
-            onClick={handleEditClick}
-            className="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-med px-6 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Edit
-          </button>
+              type="button"
+              onClick={handleEditClick}
+              className="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-med px-6 py-2 3xl:px-8 3xl:py-3 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Edit
+            </button>
           )}
+        </div>
+      </div>
+      <div className="lg:flex justify-center lg:justify-end 3xl:justify-center col-start-1 col-span-1 lg:col-span-2 lg:col-start-4 lg:row-span-2 lg:row-start-1 3xl:col-start-5 hidden lg:block">
+        <div
+          className="border border-gray-500 w-[250px] lg:w-[280px] 3xl:w-[400px] h-fit max-w-fit lg:mr-5 3xl:mr-0 mt-4 lg:mt-0"
+          style={{ borderRadius: "20px" }}
+        >
+          <Image
+            src={eye}
+            alt="eye"
+            width={0}
+            height={0}
+            className="h-52 w-52 3xl:h-80 3xl:w-80"
+          />
+          <div
+            className="text-2xl 3xl:text-3xl text-bold ml-4 mr-3 my-2 lg:mr-5 lg:ml-6 3xl:mr-6 3xl:ml-7 3xl:my-4"
+            style={{ fontWeight: "bold" }}
+          >
+            What info is shared with others?
+          </div>
+          <div className="text-lg 3xl:text-xl ml-4 mr-3 my-2 lg:mr-5 lg:ml-6 3xl:mr-6 3xl:ml-7 3xl:my-4">
+            Sports Hub only releases contact information to the host of the
+            event you are attending.
+          </div>
         </div>
       </div>
     </div>
   );
-  };
-  
-  export default Profile;
+};
+
+export default Profile;
