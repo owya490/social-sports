4797b1d035097e7106055495afc66427183feae1
Edwin <e.ni@student.unsw.edu.au>
2024-01-17 22:56:59 +1100

    Small fixes

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -112,6 +112,7 @@ export default function Login() {
             <button
               type="submit"
               className="flex w-full justify-center rounded-md bg-[#30ADFF] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+              tabIndex={3}
             >
               Sign in
             </button>
--- frontend/components/navbar/Navbar.tsx
+++ frontend/components/navbar/Navbar.tsx
@@ -1,18 +1,10 @@
 "use client";
 
-import { useState } from "react";
 import Logo from "./Logo";
 import ProfilePic from "./ProfilePic";
 import SearchBar from "./SearchBar";
-import { useRouter } from "next/navigation";
 
 export default function Navbar() {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-  const router = useRouter();
-
   return (
     <div className="bg-white drop-shadow-lg fixed top-0 w-screen z-50">
       <div className="flex items-center py-2 px-5 lg:px-8 xl:px-12">
