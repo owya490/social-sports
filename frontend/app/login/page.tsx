5cfdae2d1325a0b80e2f04b05e97a4285d73e049
Edwin <e.ni@student.unsw.edu.au>
2024-01-19 07:41:49 +1100

    Commenting login page to fix deployment

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,134 +1,134 @@
-"use client";
-import { handleEmailAndPasswordSignIn } from "@/services/authService";
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
-import { Alert } from "@material-tailwind/react";
-import Link from "next/link";
-import { useUser } from "@/components/utility/UserContext";
+// "use client";
+// import { handleEmailAndPasswordSignIn } from "@/services/authService";
+// import React, { useState } from "react";
+// import { useRouter } from "next/navigation";
+// import { Alert } from "@material-tailwind/react";
+// import Link from "next/link";
+// import { useUser } from "@/components/utility/UserContext";
 
-export default function Login() {
-  const [userData, setUserData] = useState({
-    email: "",
-    password: "",
-  });
-  const [showLoginFailure, setShowLoginFailure] = useState(false);
-  const router = useRouter();
+// export default function Login() {
+//   const [userData, setUserData] = useState({
+//     email: "",
+//     password: "",
+//   });
+//   const [showLoginFailure, setShowLoginFailure] = useState(false);
+//   const router = useRouter();
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
 
-    try {
-      await handleEmailAndPasswordSignIn(userData.email, userData.password);
-      router.push("/dashboard?login=success");
-    } catch (error) {
-      setShowLoginFailure(true);
-      console.error("Error:", error);
-    }
-  };
+//     try {
+//       await handleEmailAndPasswordSignIn(userData.email, userData.password);
+//       router.push("/dashboard?login=success");
+//     } catch (error) {
+//       setShowLoginFailure(true);
+//       console.error("Error:", error);
+//     }
+//   };
 
-  return (
-    <div className="flex p-6 min-h-[100vh] flex-1 flex-col mt-20 sm:mt-40">
-      <Alert
-        open={showLoginFailure}
-        onClose={() => setShowLoginFailure(false)}
-        color="red"
-        className="absolute ml-auto mr-auto left-0 right-0 top-24 w-fit"
-      >
-        Wrong email or password!
-      </Alert>
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h1 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 mt-[10vh] sm:mt-0">
-          Sign in to your account
-        </h1>
-      </div>
+//   return (
+//     <div className="flex p-6 min-h-[100vh] flex-1 flex-col mt-20 sm:mt-40">
+//       <Alert
+//         open={showLoginFailure}
+//         onClose={() => setShowLoginFailure(false)}
+//         color="red"
+//         className="absolute ml-auto mr-auto left-0 right-0 top-24 w-fit"
+//       >
+//         Wrong email or password!
+//       </Alert>
+//       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+//         <h1 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 mt-[10vh] sm:mt-0">
+//           Sign in to your account
+//         </h1>
+//       </div>
 
-      <div className="mt-8 mx-auto w-full max-w-sm">
-        <form className="space-y-6" onSubmit={handleSubmit}>
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                tabIndex={1}
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#30ADFF] sm:text-sm sm:leading-6"
-                value={userData.email}
-                onChange={(e) =>
-                  setUserData({
-                    ...userData,
-                    email: e.target.value,
-                  })
-                }
-              />
-            </div>
-          </div>
+//       <div className="mt-8 mx-auto w-full max-w-sm">
+//         <form className="space-y-6" onSubmit={handleSubmit}>
+//           <div>
+//             <label
+//               htmlFor="email"
+//               className="block text-sm font-medium leading-6 text-gray-900"
+//             >
+//               Email address
+//             </label>
+//             <div className="mt-2">
+//               <input
+//                 id="email"
+//                 name="email"
+//                 type="email"
+//                 autoComplete="email"
+//                 tabIndex={1}
+//                 required
+//                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#30ADFF] sm:text-sm sm:leading-6"
+//                 value={userData.email}
+//                 onChange={(e) =>
+//                   setUserData({
+//                     ...userData,
+//                     email: e.target.value,
+//                   })
+//                 }
+//               />
+//             </div>
+//           </div>
 
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-              <div className="text-sm">
-                <Link
-                  href="#"
-                  className="font-semibold text-[#30ADFF] hover:underline"
-                >
-                  Forgot password?
-                </Link>
-              </div>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                tabIndex={2}
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#30ADFF] sm:text-sm sm:leading-6"
-                value={userData.password}
-                onChange={(e) =>
-                  setUserData({
-                    ...userData,
-                    password: e.target.value,
-                  })
-                }
-              />
-            </div>
-          </div>
+//           <div>
+//             <div className="flex items-center justify-between">
+//               <label
+//                 htmlFor="password"
+//                 className="block text-sm font-medium leading-6 text-gray-900"
+//               >
+//                 Password
+//               </label>
+//               <div className="text-sm">
+//                 <Link
+//                   href="#"
+//                   className="font-semibold text-[#30ADFF] hover:underline"
+//                 >
+//                   Forgot password?
+//                 </Link>
+//               </div>
+//             </div>
+//             <div className="mt-2">
+//               <input
+//                 id="password"
+//                 name="password"
+//                 type="password"
+//                 autoComplete="current-password"
+//                 tabIndex={2}
+//                 required
+//                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#30ADFF] sm:text-sm sm:leading-6"
+//                 value={userData.password}
+//                 onChange={(e) =>
+//                   setUserData({
+//                     ...userData,
+//                     password: e.target.value,
+//                   })
+//                 }
+//               />
+//             </div>
+//           </div>
 
-          <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-[#30ADFF] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-              tabIndex={3}
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
+//           <div>
+//             <button
+//               type="submit"
+//               className="flex w-full justify-center rounded-md bg-[#30ADFF] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+//               tabIndex={3}
+//             >
+//               Sign in
+//             </button>
+//           </div>
+//         </form>
 
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Not a member?{" "}
-          <Link
-            href="/register"
-            className="font-semibold leading-6 text-[#30ADFF] hover:underline"
-          >
-            Register here
-          </Link>
-        </p>
-      </div>
-    </div>
-  );
-}
+//         <p className="mt-10 text-center text-sm text-gray-500">
+//           Not a member?{" "}
+//           <Link
+//             href="/register"
+//             className="font-semibold leading-6 text-[#30ADFF] hover:underline"
+//           >
+//             Register here
+//           </Link>
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
