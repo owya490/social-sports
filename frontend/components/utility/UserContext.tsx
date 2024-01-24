5c5803e69dd126bed4f6b193fd169ae90d8ce703
Edwin <e.ni@student.unsw.edu.au>
2024-01-25 01:56:20 +1100

    Properly added user information into Context

--- frontend/components/utility/UserContext.tsx
+++ frontend/components/utility/UserContext.tsx
@@ -4,13 +4,33 @@ import { auth, db } from "@/services/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 
-export const LoginUserContext = createContext({});
+type LoginUserContextType = {
+  user: UserType;
+  setUser: React.Dispatch<React.SetStateAction<UserType>>;
+};
+
+export const LoginUserContext = createContext<LoginUserContextType>({
+  user: null,
+  setUser: () => {},
+});
 
 // TODO: Add more fields of what you want to be in the context
-type UserType = {
-  uid: string;
-  email: string | null;
-} | null;
+type UserDocType = {
+  profilePic: string;
+  firstName: string;
+  lastName: string;
+  mobile: string;
+  dob: string;
+  location: string;
+  sport: string;
+};
+
+type UserType =
+  | (UserDocType & {
+      uid: string;
+      email: string | null;
+    })
+  | null;
 
 export default function UserContext({ children }: { children: any }) {
   const [user, setUser] = useState<UserType>(null);
@@ -22,11 +42,27 @@ export default function UserContext({ children }: { children: any }) {
           const { uid, email } = userAuth;
           const userDocRef = await doc(db, "Users", uid);
           const userDoc = await getDoc(userDocRef);
-          setUser({ uid, email });
-          if (userDoc.exists()) {
-            console.log("Userdoc: ", userDoc.data());
-            console.log("UserAuth Obj: ", userAuth);
-          }
+          const userData = userDoc.data() as UserDocType;
+          const {
+            profilePic,
+            firstName,
+            lastName,
+            mobile,
+            dob,
+            location,
+            sport,
+          } = userData;
+          setUser({
+            uid,
+            email,
+            profilePic,
+            firstName,
+            lastName,
+            mobile,
+            dob,
+            location,
+            sport,
+          });
         } else setUser(null);
       } catch (error) {
         // Most probably a connection error. Handle appropriately.
