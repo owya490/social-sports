2046cdf32ce248afe776a498b2a60c5ff1dc5327
Brian Wang <brian.wang@wisetechglobal.com>
2024-03-11 00:58:24 +1100

    added stub other pages

--- frontend/app/organiser/event/[id]/page.tsx
+++ frontend/app/organiser/event/[id]/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import EventBanner from "@/components/events/EventBanner";
+import EventDrilldownCommunicationPage from "@/components/organiser/EventDrilldownCommunicationPage";
 import EventDrilldownDetailsPage from "@/components/organiser/EventDrilldownDetailsPage";
 import EventDrilldownManageAttendeesPage from "@/components/organiser/EventDrilldownManageAttendeesPage";
+import EventDrilldownSharePage from "@/components/organiser/EventDrilldownSharePage";
 import EventDrilldownSidePanel from "@/components/organiser/EventDrilldownSidePanel";
 import EventDrilldownStatBanner from "@/components/organiser/EventDrilldownStatBanner";
 import OrganiserNavbar from "@/components/organiser/OrganiserNavbar";
@@ -22,8 +24,12 @@ export default function EventPage() {
         <div>
           <EventDrilldownSidePanel setCurrSidebarPage={setCurrSidebarPage} />
         </div>
-        <div className="mx-auto">{currSidebarPage === "Details" && <EventDrilldownDetailsPage />}</div>
-        <div className="mx-auto">{currSidebarPage === "Manage Attendees" && <EventDrilldownManageAttendeesPage />}</div>
+        <div className="mx-auto w-full mx-20">
+          {currSidebarPage === "Details" && <EventDrilldownDetailsPage />}
+          {currSidebarPage === "Manage Attendees" && <EventDrilldownManageAttendeesPage />}
+          {currSidebarPage === "Communication" && <EventDrilldownCommunicationPage />}
+          {currSidebarPage === "Share" && <EventDrilldownSharePage />}
+        </div>
       </div>
     </div>
   );
---
+++ frontend/components/organiser/EventDrilldownSharePage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EventDrilldownSharePage = () => {
+  return (
+    <div className="flex flex-col space-y-4 mb-6 w-full">
+      <div className="text-4xl font-extrabold">Share</div>
+    </div>
+  );
+};
+
+export default EventDrilldownSharePage;
