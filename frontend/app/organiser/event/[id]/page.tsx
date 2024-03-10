f2796750675a602c94c42ba9334ccadfe108b969
Brian Wang <brian.wang@wisetechglobal.com>
2024-03-10 13:08:25 +1100

    added conditionals

--- frontend/app/organiser/event/[id]/page.tsx
+++ frontend/app/organiser/event/[id]/page.tsx
@@ -1,12 +1,18 @@
+"use client";
+
 import EventBanner from "@/components/events/EventBanner";
 import EventDrilldownDetailsPage from "@/components/organiser/EventDrilldownDetailsPage";
+import EventDrilldownManageAttendeesPage from "@/components/organiser/EventDrilldownManageAttendeesPage";
 import EventDrilldownSidePanel from "@/components/organiser/EventDrilldownSidePanel";
 import EventDrilldownStatBanner from "@/components/organiser/EventDrilldownStatBanner";
 import OrganiserNavbar from "@/components/organiser/OrganiserNavbar";
 import { Timestamp } from "firebase/firestore";
+import { useState } from "react";
 
 //brians
 export default function EventPage() {
+  const [currSidebarPage, setCurrSidebarPage] = useState("Details");
+
   return (
     <div className="ml-14 mt-16">
       <OrganiserNavbar />
@@ -14,11 +20,10 @@ export default function EventPage() {
       <EventDrilldownStatBanner />
       <div className="flex flex-row">
         <div>
-          <EventDrilldownSidePanel />
+          <EventDrilldownSidePanel setCurrSidebarPage={setCurrSidebarPage} />
         </div>
-        <div className="mx-auto">
-          <EventDrilldownDetailsPage />
-        </div>
+        <div className="mx-auto">{currSidebarPage === "Details" && <EventDrilldownDetailsPage />}</div>
+        <div className="mx-auto">{currSidebarPage === "Manage Attendees" && <EventDrilldownManageAttendeesPage />}</div>
       </div>
     </div>
   );
--- frontend/components/organiser/EventDrilldownSidePanel.tsx
+++ frontend/components/organiser/EventDrilldownSidePanel.tsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
-const EventDrilldownSidePanel = () => {
+interface EventDrilldownSidePanelProps {
+  setCurrSidebarPage: Dispatch<SetStateAction<string>>;
+}
+
+const EventDrilldownSidePanel = ({ setCurrSidebarPage }: EventDrilldownSidePanelProps) => {
   return (
     <div className="bg-organiser-light-gray mx-10 p-8 w-64 rounded-3xl">
       <div className="text-xl font-extrabold">Volleyball World Cup</div>
       <div className="text-organiser-dark-gray-text text-sm font-bold">Sun, 17 March 2024, 10:00am</div>
       <div className="h-20"></div>
       <div className="flex flex-col space-y-6">
-        <div className="text-organiser-dark-gray-text font-bold text-md">Details</div>
-        <div className="text-organiser-dark-gray-text font-bold text-md">Manage Attendees</div>
-        <div className="text-organiser-dark-gray-text font-bold text-md">Communication</div>
-        <div className="text-organiser-dark-gray-text font-bold text-md">Share</div>
+        <div className="text-organiser-dark-gray-text font-bold text-md" onClick={() => setCurrSidebarPage("Details")}>
+          Details
+        </div>
+        <div
+          className="text-organiser-dark-gray-text font-bold text-md"
+          onClick={() => setCurrSidebarPage("Manage Attendees")}
+        >
+          Manage Attendees
+        </div>
+        <div
+          className="text-organiser-dark-gray-text font-bold text-md"
+          onClick={() => setCurrSidebarPage("Communication")}
+        >
+          Communication
+        </div>
+        <div className="text-organiser-dark-gray-text font-bold text-md" onClick={() => setCurrSidebarPage("Share")}>
+          Share
+        </div>
       </div>
       <div className="h-40"></div>
     </div>
