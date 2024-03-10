dedab42b85d3f32458110d34d9c9fed3add095b6
Brian Wang <brian.wang@wisetechglobal.com>
2024-03-10 12:36:04 +1100

    finished off stat banner

--- frontend/components/organiser/EventDrilldownStatBanner.tsx
+++ frontend/components/organiser/EventDrilldownStatBanner.tsx
@@ -4,17 +4,19 @@ const EventDrilldownStatBanner = () => {
   return (
     <div>
       <div className="bg-organiser-light-gray p-16 m-10 rounded-3xl flex justify-between flex-row">
-        <div className="text-center">
-          <div>Net Sales</div>
-          <div>A$50.12</div>
+        <div className="text-center basis-1/4">
+          <div className="text-lg">Net Sales</div>
+          <div className="font-extrabold text-3xl">A$50.12</div>
         </div>
-        <div className="text-center">
-          <div>Tickets Sold</div>
-          <div>4/20</div>
+        <div className="inline-block h-20 w-0.5 self-stretch bg-organiser-title-gray-text"></div>
+        <div className="text-center basis-1/4">
+          <div className="text-lg">Tickets Sold</div>
+          <div className="font-extrabold text-3xl">4/20</div>
         </div>
-        <div className="text-center">
-          <div>Page Views</div>
-          <div>72</div>
+        <div className="inline-block h-20 w-0.5 self-stretch bg-organiser-title-gray-text"></div>
+        <div className="text-center basis-1/4">
+          <div className="text-lg">Page Views</div>
+          <div className="font-extrabold text-3xl">72</div>
         </div>
       </div>
     </div>
