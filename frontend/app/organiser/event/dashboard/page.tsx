3f8a90857cc826f910f644959c5293db08252388
unknown <aidanyschee@gmail.com>
2024-03-13 16:52:53 +1100

    filters

--- frontend/app/organiser/event/dashboard/page.tsx
+++ frontend/app/organiser/event/dashboard/page.tsx
@@ -3,44 +3,104 @@ import { useState } from "react";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/20/solid";
 
 export default function Dashboard() {
-  const [showEventDate, setShowEventDate] = useState(false);
-  const [selectedEventDate, setSelectedEventDate] = useState<string[]>([]);
+  const [showSearch, setShowSearch] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+  const [showEventStatus, setShowEventStatus] = useState(true);
+  const [selectedEventStatus, setSelectedEventStatus] = useState<string[]>([]);
+  const [showEventType, setShowEventType] = useState(true);
+  const [selectedEventType, setSelectedEventType] = useState<string[]>([]);
+  const [showPriceRange, setShowPriceRange] = useState(true);
+  const [minPrice, setMinPrice] = useState<string>("");
+  const [maxPrice, setMaxPrice] = useState<string>("");
+  const [showDateRange, setShowDateRange] = useState(true);
+  const [startDate, setStartDate] = useState<string>("");
+  const [endDate, setEndDate] = useState<string>("");
 
-  const toggleShowEventDate = () => {
-    setShowEventDate(!showEventDate);
+  const toggleShowSearch = () => {
+    setShowSearch(!showSearch);
+  };
+  const toggleShowEventStatus = () => {
+    setShowEventStatus(!showEventStatus);
+  };
+  const toggleShowEventType = () => {
+    setShowEventType(!showEventType);
+  };
+  const toggleShowPriceRange = () => {
+    setShowPriceRange(!showPriceRange);
+  };
+  const toggleShowDateRange = () => {
+    setShowDateRange(!showDateRange);
   };
 
-  const handleEventDateChange = (value: string) => {
-    const index = selectedEventDate.indexOf(value);
+  const handleEventStatusChange = (value: string) => {
+    const index = selectedEventStatus.indexOf(value);
     if (index === -1) {
-      setSelectedEventDate([...selectedEventDate, value]);
+      setSelectedEventStatus([...selectedEventStatus, value]);
     } else {
-      const updatedEventDate = [...selectedEventDate];
-      updatedEventDate.splice(index, 1);
-      setSelectedEventDate(updatedEventDate);
+      const updatedEventStatus = [...selectedEventStatus];
+      updatedEventStatus.splice(index, 1);
+      setSelectedEventStatus(updatedEventStatus);
+    }
+  };
+  const handleEventTypeChange = (value: string) => {
+    const index = selectedEventType.indexOf(value);
+    if (index === -1) {
+      setSelectedEventType([...selectedEventType, value]);
+    } else {
+      const updatedEventType = [...selectedEventType];
+      updatedEventType.splice(index, 1);
+      setSelectedEventType(updatedEventType);
     }
   };
 
+  const clearFilters = () => {
+    setSearchValue("");
+    setSelectedEventStatus([]);
+    setSelectedEventType([]);
+    setMinPrice("");
+    setMaxPrice("");
+    setStartDate("");
+    setEndDate("");
+  };
+
   return (
-    <div className="w-screen flex justify-center mt-24">
-      <div className="w-screen mx-10 sm:mx-0 sm:w-[400px] md:w-[700px] lg:w-[1000px] xl:w-[1000px] 3xl:w-[1400px]">
-        <div className="text-5xl my-4">Event Dashboard</div>
+    <div className="w-screen flex justify-center mt-16 ml-14">
+      {/* <OrganiserNavbar /> */}
+      <div className="w-screen mx-10">
+        <div className="text-6xl my-12">Event Dashboard</div>
         <div className="flex flex-cols-3">
-          <div className="col-start-1 col-span-1">
+          <div className="col-start-1 col-span-1 max-w-sm">
             <div className="p-4 bg-gray-100 border border-gray-300 rounded-lg">
               <h2 className="text-lg text-center font-semibold mb-4">Filter Events</h2>
               <div className="mb-4">
-                <label className="block text-sm font-medium text-gray-700 mb-1">
-                  Event Date
-                  <span className="cursor-pointer ml-2" onClick={toggleShowEventDate}>
-                    {showEventDate ? (
-                      <ChevronDownIcon className="h-6 w-6 flex justify-right" />
-                    ) : (
-                      <ChevronUpIcon className="h-6 w-6" />
-                    )}
+                <label className="block text-sm font-medium text-gray-700 mb-1 flex items-center">
+                  Search
+                  <span className="cursor-pointer ml-auto" onClick={toggleShowSearch}>
+                    {showSearch ? <ChevronUpIcon className="h-6 w-6" /> : <ChevronDownIcon className="h-6 w-6" />}
                   </span>
                 </label>
-                {showEventDate && (
+                {showSearch && (
+                  <div className="mb-4">
+                    <input
+                      type="text"
+                      id="search"
+                      name="search"
+                      className="w-full p-2 border rounded-xl focus:outline-none focus:ring focus:border-blue-300"
+                      placeholder="Search for anything"
+                      value={searchValue}
+                      onChange={(e) => setSearchValue(e.target.value)}
+                    />
+                  </div>
+                )}
+              </div>
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700 mb-1 flex items-center">
+                  Event Status
+                  <span className="cursor-pointer ml-auto" onClick={toggleShowEventStatus}>
+                    {showEventStatus ? <ChevronUpIcon className="h-6 w-6" /> : <ChevronDownIcon className="h-6 w-6" />}
+                  </span>
+                </label>
+                {showEventStatus && (
                   <div>
                     <div className="mb-2">
                       <label className="inline-flex items-center">
@@ -49,8 +109,8 @@ export default function Dashboard() {
                           name="Past"
                           value="Past"
                           className="mr-2"
-                          checked={selectedEventDate.includes("Past")}
-                          onChange={() => handleEventDateChange("Past")}
+                          checked={selectedEventStatus.includes("Past")}
+                          onChange={() => handleEventStatusChange("Past")}
                         />
                         Past
                       </label>
@@ -62,8 +122,8 @@ export default function Dashboard() {
                           name="Future"
                           value="Future"
                           className="mr-2"
-                          checked={selectedEventDate.includes("Future")}
-                          onChange={() => handleEventDateChange("Future")}
+                          checked={selectedEventStatus.includes("Future")}
+                          onChange={() => handleEventStatusChange("Future")}
                         />
                         Future
                       </label>
@@ -72,43 +132,118 @@ export default function Dashboard() {
                 )}
               </div>
               <div className="mb-4">
-                <label htmlFor="priceRange" className="block text-sm font-medium text-gray-700 mb-1">
-                  Price Range:
+                <label className="block text-sm font-medium text-gray-700 mb-1 flex items-center">
+                  Event Type
+                  <span className="cursor-pointer ml-auto" onClick={toggleShowEventType}>
+                    {showEventType ? <ChevronUpIcon className="h-6 w-6" /> : <ChevronDownIcon className="h-6 w-6" />}
+                  </span>
                 </label>
-                <input
-                  type="range"
-                  id="priceRange"
-                  name="priceRange"
-                  min="0"
-                  max="1000"
-                  step="10"
-                  className="w-full focus:outline-none focus:ring focus:border-blue-300"
-                />
-                <div className="flex justify-between">
-                  <span>$0</span>
-                  <span>$1000</span>
-                </div>
+                {showEventType && (
+                  <div>
+                    <div className="mb-2">
+                      <label className="inline-flex items-center">
+                        <input
+                          type="checkbox"
+                          name="Public"
+                          value="Public"
+                          className="mr-2"
+                          checked={selectedEventType.includes("Public")}
+                          onChange={() => handleEventTypeChange("Public")}
+                        />
+                        Public
+                      </label>
+                    </div>
+                    <div className="mb-2">
+                      <label className="inline-flex items-center">
+                        <input
+                          type="checkbox"
+                          name="Private"
+                          value="Private"
+                          className="mr-2"
+                          checked={selectedEventType.includes("Private")}
+                          onChange={() => handleEventTypeChange("Private")}
+                        />
+                        Private
+                      </label>
+                    </div>
+                  </div>
+                )}
               </div>
-
               <div className="mb-4">
-                <label htmlFor="brand" className="block text-sm font-medium text-gray-700 mb-1">
-                  Brand:
+                <label className="block text-sm font-medium text-gray-700 mb-1 flex items-center">
+                  Price Range
+                  <span className="cursor-pointer ml-auto" onClick={toggleShowPriceRange}>
+                    {showPriceRange ? <ChevronUpIcon className="h-6 w-6" /> : <ChevronDownIcon className="h-6 w-6" />}
+                  </span>
                 </label>
-                <input
-                  type="text"
-                  id="brand"
-                  name="brand"
-                  className="w-full p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-                  placeholder="Enter brand"
-                />
+                {showPriceRange && (
+                  <div className="flex justify-center items-center">
+                    <span className="text-gray-700 mr-2">$</span>
+                    <input
+                      type="text"
+                      id="minPrice"
+                      name="minPrice"
+                      placeholder="Min"
+                      className="w-full p-2 border rounded-xl focus:outline-none focus:ring focus:border-blue-300"
+                      value={minPrice}
+                      onChange={(e) => setMinPrice(e.target.value.replace(/[^0-9.]/g, ""))}
+                    />
+                    <span className="text-gray-700 mx-4">to</span>
+                    <span className="text-gray-700 mr-2">$</span>
+                    <input
+                      type="text"
+                      id="maxPrice"
+                      name="maxPrice"
+                      placeholder="Max"
+                      className="w-full p-2 border rounded-xl focus:outline-none focus:ring focus:border-blue-300"
+                      value={maxPrice}
+                      onChange={(e) => setMaxPrice(e.target.value.replace(/[^0-9.]/g, ""))}
+                    />
+                  </div>
+                )}
               </div>
-
-              <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">
-                Apply Filters
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700 mb-1 flex items-center">
+                  Date Range
+                  <span className="cursor-pointer ml-auto" onClick={toggleShowDateRange}>
+                    {showDateRange ? <ChevronUpIcon className="h-6 w-6" /> : <ChevronDownIcon className="h-6 w-6" />}
+                  </span>
+                </label>
+                {showDateRange && (
+                  <div className="flex justify-start items-center">
+                    <input
+                      type="date"
+                      id="startDate"
+                      name="startDate"
+                      className="w-full p-2 border rounded-xl focus:outline-none focus:ring focus:border-blue-300"
+                      placeholder="Start Date"
+                      value={startDate}
+                      onChange={(e) => setStartDate(e.target.value)}
+                    />
+                    <span className="text-gray-700 mx-4">to</span>
+                    <input
+                      type="date"
+                      id="endDate"
+                      name="endDate"
+                      className="w-full p-2 border rounded-xl focus:outline-none focus:ring focus:border-blue-300"
+                      placeholder="End Date"
+                      value={endDate}
+                      onChange={(e) => setEndDate(e.target.value)}
+                    />
+                  </div>
+                )}
+              </div>
+              <button
+                className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                onClick={clearFilters}
+              >
+                Clear Filters
               </button>
             </div>
           </div>
-          <div className="col-start-2 col-span-2">hi2</div>
+          <div className="col-start-2 col-span-2">
+            <div className="">lol</div>
+          </div>
         </div>
       </div>
     </div>
