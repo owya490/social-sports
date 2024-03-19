bc358ba311f2f18a503fe089e4cebd02d92e7b14
Brian Wang <brian.wang@wisetechglobal.com>
2024-03-20 08:41:49 +1100

    continued settings

--- frontend/components/organiser/OrganiserSettingsStripeCard.tsx
+++ frontend/components/organiser/OrganiserSettingsStripeCard.tsx
@@ -9,12 +9,23 @@ const OrganiserSettingsStripeCard = () => {
       <div>
         <div className="border-organiser-darker-light-gray border-solid border-2 rounded-3xl px-4 pt-2 relative">
           <Image src={StripeLogo} alt="..." className="w-28" />
-          <div className="p-4">
-            <span>
+          <div className="p-4 space-y-4">
+            <span className="inline-block">
               <div className="text-organiser-title-gray-text font-bold text-lg underline">Your Stripe Account</div>
-              <ArrowTopRightOnSquareIcon className="w-6" />
+              <ArrowTopRightOnSquareIcon className="w-4" />
             </span>
-            <div className="font-bold text-2xl">Volleyball World Cup</div>
+            <div>
+              <div className="font-bold text-xl text-organiser-title-gray-text">Stripe Account ID</div>
+              <div className="border-organiser-darker-light-gray border-solid border-2 rounded-3xl pl-4 p-2 relative w-3/4">
+                12365237512375
+              </div>
+            </div>
+            <div>
+              <div className="font-bold text-xl text-organiser-title-gray-text">Stripe Payment Method</div>
+              <div className="border-organiser-darker-light-gray border-solid border-2 rounded-3xl pl-4 p-2 relative w-3/4">
+                PayPal
+              </div>
+            </div>
           </div>
         </div>
         <div></div>
