e707a68a09102f559cfd8a8a6128070764ae5822
Brian Wang <brian.wang@wisetechglobal.com>
2024-03-25 08:52:50 +1100

    removed author of event on event drilldown banner

--- frontend/components/organiser/EventDrilldownBanner.tsx
+++ frontend/components/organiser/EventDrilldownBanner.tsx
@@ -13,7 +13,7 @@ interface EventBannerProps {
 
 const EventDrilldownBanner = ({ name, startDate, organiser, vacancy }: EventBannerProps) => {
   return (
-    <div className="bg-organiser-light-gray border-b-black border-0 border w-100% px-5 md:px-10 pt-0 shadow-lg flex justify-center">
+    <div className="bg-organiser-light-gray border-b-black w-100% px-5 md:px-10 pt-2 shadow-lg flex justify-center">
       <div className="screen-width-primary">
         <div className="flex items-center">
           <div className="mt-3">
@@ -21,19 +21,6 @@ const EventDrilldownBanner = ({ name, startDate, organiser, vacancy }: EventBann
             <h1 className="text-3xl md:text-4xl">{name}</h1>
 
             <div className="block md:flex items-center pt-2 pb-4 pl-1">
-              <div className="flex items-center">
-                <Image
-                  src={organiser.profilePicture}
-                  alt="DP"
-                  width={50}
-                  height={50}
-                  className="rounded-full w-4 h-4"
-                />
-                <p className="text-xs font-light ml-1 mr-4">
-                  {`Hosted by ${organiser.firstName} ${organiser.surname}`}
-                </p>
-              </div>
-
               <p className="font-bold text-xs hidden md:block">{timestampToEventCardDateString(startDate)}</p>
             </div>
           </div>
