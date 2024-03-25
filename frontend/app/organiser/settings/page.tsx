5d635bf57088ded036ed6dc25071222001349c29
Brian Wang <brian.wang@wisetechglobal.com>
2024-03-25 17:19:46 +1100

    added links to organiser navbar and polished up margining for organiser settings

--- frontend/app/organiser/settings/page.tsx
+++ frontend/app/organiser/settings/page.tsx
@@ -5,9 +5,11 @@ import React from "react";
 const page = () => {
   return (
     <div className="ml-14 mt-16">
-      <OrganiserNavbar />
-      <div className="p-10">
-        <OrganiserSettingsStripeCard />
+      <div className="max-w-5xl lg:mx-auto">
+        <OrganiserNavbar />
+        <div className="p-10">
+          <OrganiserSettingsStripeCard />
+        </div>
       </div>
     </div>
   );
--- frontend/components/organiser/OrganiserNavbar.tsx
+++ frontend/components/organiser/OrganiserNavbar.tsx
@@ -14,32 +14,32 @@ export default function OrganiserNavbar() {
   return (
     <div className="bg-organiser-light-gray drop-shadow-lg fixed left-0 h-screen z-40">
       <div className="w-14 flex flex-col mt-14 space-y-10">
-        <Link href="/organiser/dashboard">
+        <Link href="/organiser/dashboard/">
           <div className="flex justify-center">
             <HomeIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
         </Link>
-        <Link href="/organiser/dashboard">
+        <Link href="/organiser/event">
           <div className="flex justify-center">
             <CalendarIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
         </Link>
-        <Link href="/organiser/dashboard">
+        <Link href="/organiser/event/asdf">
           <div className="flex justify-center">
             <BookmarkSquareIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
         </Link>
-        <Link href="/organiser/dashboard">
+        <Link href="/organiser/metrics">
           <div className="flex justify-center">
             <ChartBarIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
         </Link>
-        <Link href="/organiser/dashboard">
+        <Link href="/organiser/gallery">
           <div className="flex justify-center">
             <CameraIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
         </Link>
-        <Link href="/organiser/dashboard">
+        <Link href="/organiser/settings">
           <div className="flex justify-center">
             <UserIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
