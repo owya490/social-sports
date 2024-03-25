10db7d790c9d6be5b022abdfbc537b3b868f22f0
Brian Wang <brian.wang@wisetechglobal.com>
2024-03-25 17:34:39 +1100

    added highlight onto organiser navbar

--- frontend/components/organiser/EventDrilldownAttendeeCard.tsx
+++ frontend/components/organiser/EventDrilldownAttendeeCard.tsx
@@ -11,7 +11,7 @@ interface EventDrilldownAttendeeCardProps {
 const EventDrilldownAttendeeCard = ({ tickets, name, email, number }: EventDrilldownAttendeeCardProps) => {
   return (
     <div className="grid grid-flow-col justify-stretch py-2 grid-cols-7 flex items-center">
-      <div className="col-span-1">{tickets}</div>
+      <div className="col-span-1 w-14 text-center">{tickets}</div>
       <div className="flex flex-row items-center col-span-2">
         <UserCircleIcon className="stroke-1 w-10 mr-2" />
         <div className="">{name}</div>
--- frontend/components/organiser/OrganiserNavbar.tsx
+++ frontend/components/organiser/OrganiserNavbar.tsx
@@ -13,34 +13,34 @@ import Link from "next/link";
 export default function OrganiserNavbar() {
   return (
     <div className="bg-organiser-light-gray drop-shadow-lg fixed left-0 h-screen z-40">
-      <div className="w-14 flex flex-col mt-14 space-y-10">
+      <div className="w-14 flex flex-col mt-14 space-y-3">
         <Link href="/organiser/dashboard/">
-          <div className="flex justify-center">
+          <div className="flex justify-center h-10 w-10 m-auto rounded-md hover:bg-organiser-darker-light-gray transition ease-in-out">
             <HomeIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
         </Link>
         <Link href="/organiser/event">
-          <div className="flex justify-center">
+          <div className="flex justify-center h-10 w-10 m-auto rounded-md hover:bg-organiser-darker-light-gray transition ease-in-out">
             <CalendarIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
         </Link>
         <Link href="/organiser/event/asdf">
-          <div className="flex justify-center">
+          <div className="flex justify-center h-10 w-10 m-auto rounded-md hover:bg-organiser-darker-light-gray transition ease-in-out">
             <BookmarkSquareIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
         </Link>
         <Link href="/organiser/metrics">
-          <div className="flex justify-center">
+          <div className="flex justify-center h-10 w-10 m-auto rounded-md hover:bg-organiser-darker-light-gray transition ease-in-out">
             <ChartBarIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
         </Link>
         <Link href="/organiser/gallery">
-          <div className="flex justify-center">
+          <div className="flex justify-center h-10 w-10 m-auto rounded-md hover:bg-organiser-darker-light-gray transition ease-in-out">
             <CameraIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
         </Link>
         <Link href="/organiser/settings">
-          <div className="flex justify-center">
+          <div className="flex justify-center h-10 w-10 m-auto rounded-md hover:bg-organiser-darker-light-gray transition ease-in-out">
             <UserIcon className="w-6 stroke-1 stroke-organiser-dark-gray-text" />
           </div>
         </Link>
