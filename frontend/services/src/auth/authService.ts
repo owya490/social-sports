da9d8b894e779e0586283de6370c34541da204b6
inzaenity <118352275+inzaenity@users.noreply.github.com>
2024-05-07 23:40:51 +1000

    stuff:

--- frontend/services/src/auth/authService.ts
+++ frontend/services/src/auth/authService.ts
@@ -9,7 +9,8 @@ import {
   signInWithPopup,
   signOut,
   getAuth,
-  sendPasswordResetEmail
+  sendPasswordResetEmail,
+  sendSignInLinkToEmail,
 } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import { useRouter } from "next/navigation";
@@ -20,24 +21,33 @@ const authServiceLogger = new Logger("authServiceLogger");
 
 export async function handleEmailAndPasswordSignUp(data: NewUserData) {
   try {
-    // Create a new user with email and password
-    console.log(data);
-    const userCredential: UserCredential = await createUserWithEmailAndPassword(
-      auth,
-      data.contactInformation.email,
-      data.password
-    );
-    console.log(userCredential.user.uid);
-    if (userCredential.user) {
-      // TODO: we need to check firebase auth if the user is already created, if they are, we don't recreate the user.. figure smth out
-      createUser(data, userCredential.user.uid);
-    } else {
-      // Handle the case where userCredential.user is null
-      console.error("User authentication failed");
-    }
-  } catch (error) {
-    throw error;
+    await sendEmailVerification(data.contactInformation.email);
+    console.log("Verification email sent. Please check your inbox.");
+  } catch (verificationError) {
+    console.error("Failed to send verification email:", verificationError);
+    return;
   }
+
+  // try {
+  //   await sendEmailVerification(data.contactInformation.email);
+  //   // Create a new user with email and password
+  //   console.log(data);
+  //   const userCredential: UserCredential = await createUserWithEmailAndPassword(
+  //     auth,
+  //     data.contactInformation.email,
+  //     data.password
+  //   );
+  //   console.log(userCredential.user.uid);
+  //   if (userCredential.user) {
+  //     // TODO: we need to check firebase auth if the user is already created, if they are, we don't recreate the user.. figure smth out
+  //     createUser(data, userCredential.user.uid);
+  //   } else {
+  //     // Handle the case where userCredential.user is null
+  //     console.error("User authentication failed");
+  //   }
+  // } catch (error) {
+  //   throw error;
+  // }
 }
 
 export async function handleSignOut(setUser: (user: UserData) => void) {
@@ -112,16 +122,45 @@ export function isLoggedIn(): boolean {
   return authUser !== null;
 }
 
+const actionCodeSettings = {
+  // URL you want to redirect back to. The domain (www.example.com) for this
+  // URL must be in the authorized domains list in the Firebase Console.
+  url: "http://localhost:3000/dashboard",
+  // This must be true.
+  handleCodeInApp: true,
+  iOS: {
+    bundleId: "com.example.ios",
+  },
+  android: {
+    packageName: "com.example.android",
+    installApp: true,
+    minimumVersion: "12",
+  },
+  dynamicLinkDomain: "http://localhost:3000/",
+};
 
 export async function resetUserPassword(email: string): Promise<void> {
   try {
     // Send password reset email
-    sendPasswordResetEmail(auth, email)
+    sendPasswordResetEmail(auth, email);
     // Password reset email sent successfully
     console.log(`Password reset email sent to ${email}`);
   } catch (error) {
     // Handle errors
-    console.error('Error sending password reset email:', error);
+    console.error("Error sending password reset email:", error);
+    throw error; // Rethrow the error for the caller to handle if needed
+  }
+}
+
+export async function sendEmailVerification(email: string): Promise<void> {
+  try {
+    // Send email verification first
+    await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+    // Password reset email sent successfully
+    console.log(`Email Verification sent to ${email}`);
+  } catch (error) {
+    // Handle errors
+    console.error("Error sending email verification:", error);
     throw error; // Rethrow the error for the caller to handle if needed
   }
 }
