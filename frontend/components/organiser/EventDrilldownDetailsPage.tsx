6c1a6bf6e0caa7e9492556f0d1773dd993fd9455
unknown <aidanyschee@gmail.com>
2024-05-08 18:15:21 +1000

    in text editing for title

--- frontend/components/organiser/EventDrilldownDetailsPage.tsx
+++ frontend/components/organiser/EventDrilldownDetailsPage.tsx
@@ -1,8 +1,3 @@
-import Image from "next/image";
-import React from "react";
-
-import BannerImage from "../../public/images/vball1.webp";
-import EditIcon from "../../public/images/edit-icon.svg";
 import {
   CalendarDaysIcon,
   ClockIcon,
@@ -10,8 +5,31 @@ import {
   MapPinIcon,
   PencilSquareIcon,
 } from "@heroicons/react/24/outline";
+import CloseIcon from "@mui/icons-material/Close";
+import DoneIcon from "@mui/icons-material/Done";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+import Image from "next/image";
+import { useState } from "react";
+import BannerImage from "../../public/images/vball1.webp";
 
 const EventDrilldownDetailsPage = () => {
+  const [editTitle, setEditTitle] = useState(false);
+  const [newEditTitle, setNewEditTitle] = useState("Volleyball World Cup");
+  const [title, setTitle] = useState("Volleyball World Cup");
+  console.log(editTitle);
+
+  const handleTitleUpdate = () => {
+    setTitle(newEditTitle);
+    // Update to firestore
+    setEditTitle(false);
+  };
+
+  const handleCancelEdit = () => {
+    setNewEditTitle(title);
+    setEditTitle(false);
+  };
+
   return (
     <div className="flex flex-col space-y-4 mb-6">
       <div>
@@ -23,10 +41,47 @@ const EventDrilldownDetailsPage = () => {
           className="h-full w-full object-cover rounded-3xl"
         />
       </div>
-      <div className="h-20 border-organiser-darker-light-gray border-solid border-2 rounded-3xl pl-4 pt-2 relative">
-        <div className="text-organiser-title-gray-text font-bold">Event Name</div>
-        <div className="font-bold text-2xl">Volleyball World Cup</div>
-        <PencilSquareIcon className="absolute top-2 right-2 w-5 stroke-organiser-title-gray-text" />
+      <div className="h-20 border-organiser-darker-light-gray border-solid border-2 rounded-3xl px-4 pt-2 relative">
+        <div className="text-organiser-title-gray-text font-bold">
+          Event Name
+          {editTitle ? (
+            <>
+              <TextField
+                value={newEditTitle}
+                variant="standard"
+                fullWidth
+                inputProps={{ style: { fontSize: "1.25rem", color: "#333" } }} // Match styling here
+                onChange={(e) => {
+                  setNewEditTitle(e.target.value);
+                }}
+              />
+              <IconButton
+                className="absolute w-5 stroke-organiser-title-gray-text cursor-pointer"
+                onClick={() => {
+                  handleTitleUpdate();
+                }}
+              >
+                <DoneIcon />
+              </IconButton>
+              <IconButton
+                className="w-5 stroke-organiser-title-gray-text cursor-pointer"
+                onClick={() => {
+                  handleCancelEdit();
+                }}
+              >
+                <CloseIcon />
+              </IconButton>
+            </>
+          ) : (
+            <div className="font-bold text-2xl">
+              {newEditTitle}
+              <PencilSquareIcon
+                className="absolute top-2 right-2 w-5 stroke-organiser-title-gray-text cursor-pointer"
+                onClick={() => setEditTitle(true)}
+              />
+            </div>
+          )}
+        </div>
       </div>
       <div className="border-organiser-darker-light-gray border-solid border-2 rounded-3xl pl-4 p-2 pb-4 relative">
         <div className="text-organiser-title-gray-text font-bold">Event Details</div>
@@ -37,7 +92,7 @@ const EventDrilldownDetailsPage = () => {
           </div>
           <div className="px-2 flex flex-row space-x-2">
             <ClockIcon className="w-4" />
-            <div>Mon Jan 29 2024</div>
+            <div>8:00pm - 10:00pm</div>
           </div>
           <div className="px-2 flex flex-row space-x-2">
             <MapPinIcon className="w-4" />
