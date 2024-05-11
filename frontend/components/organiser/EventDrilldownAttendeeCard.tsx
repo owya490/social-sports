1fd52c4e2d37b7ac85bcb0c2fd3790e5c098c014
Brian Wang <brian.wang@wisetechglobal.com>
2024-05-11 14:58:52 +1000

    removed depracated from profilepic

--- frontend/components/organiser/EventDrilldownAttendeeCard.tsx
+++ frontend/components/organiser/EventDrilldownAttendeeCard.tsx
@@ -1,5 +1,5 @@
 import { removeAttendee } from "@/services/src/organiser/organiserService";
-import { Menu, Transition } from "@headlessui/react";
+import { Menu, MenuButton, MenuItem, MenuItems, Transition } from "@headlessui/react";
 import { EllipsisVerticalIcon, UserCircleIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import React, { Fragment, useState } from "react";
@@ -24,18 +24,17 @@ const EventDrilldownAttendeeCard = ({ tickets, name, email, number, image }: Eve
       <div className="col-span-1 w-14 text-center">{tickets}</div>
       <div className="flex flex-row items-center col-span-2">
         <Image src={image} alt="" width={100} height={100} className="w-10 rounded-full" />
-        {/* <UserCircleIcon className="stroke-1 w-10 mr-2" /> */}
         <div className="">{name}</div>
       </div>
       <div className="col-span-3">{email}</div>
       <div className={`col-span-1 ${number === null ? "text-gray-300" : ""}`}>{number === null ? "N/A" : number}</div>
       <Menu as="div" className="relative">
         <div>
-          <Menu.Button>
+          <MenuButton>
             <div className="p-1.5 cursor-pointer rounded-full hover:bg-organiser-darker-light-gray hover:ease-in transition">
               <EllipsisVerticalIcon className="w-6" />
             </div>
-          </Menu.Button>
+          </MenuButton>
         </div>
         <Transition
           as={Fragment}
@@ -46,36 +45,28 @@ const EventDrilldownAttendeeCard = ({ tickets, name, email, number, image }: Eve
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 mt-1 w-52 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
+          <MenuItems className="absolute right-0 mt-1 w-52 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
             <div className="px-1 py-1">
-              <Menu.Item>
-                {({ active }) => (
-                  <Link
-                    href="/profile"
-                    className={`${
-                      active ? "text-white bg-black" : "text-black"
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    <UserCircleIcon className="h-5 mr-2" />
-                    Profile
-                  </Link>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <div
-                    className={`${
-                      active ? "text-white bg-black" : "text-black"
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm hover:cursor-pointer`}
-                    onClick={() => setIsFilterModalOpen(true)}
-                  >
-                    <XMarkIcon className="h-5 mr-2" />
-                    Remove attendee
-                  </div>
-                )}
-              </Menu.Item>
+              <MenuItem>
+                <Link
+                  href="/profile"
+                  className={`text-black group flex w-full items-center rounded-md px-2 py-2 text-sm hover:text-white hover:bg-black`}
+                >
+                  <UserCircleIcon className="h-5 mr-2" />
+                  Profile
+                </Link>
+              </MenuItem>
+              <MenuItem>
+                <div
+                  className={`text-black group flex w-full items-center rounded-md px-2 py-2 text-sm hover:cursor-pointer hover:text-white hover:bg-black `}
+                  onClick={() => setIsFilterModalOpen(true)}
+                >
+                  <XMarkIcon className="h-5 mr-2" />
+                  Remove attendee
+                </div>
+              </MenuItem>
             </div>
-          </Menu.Items>
+          </MenuItems>
         </Transition>
       </Menu>
       <div className="grow">
