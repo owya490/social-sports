ca5a43dd401744056c2d997245c33b9579d9c29d
Brian Wang <brian.wang@student.unsw.edu.au>
2024-05-17 11:32:52 +1000

    SPORTSHUB-99 - refactored preview form

--- frontend/components/events/create/forms/PreviewForm.tsx
+++ frontend/components/events/create/forms/PreviewForm.tsx
@@ -22,55 +22,72 @@ export const PreviewForm = ({ form, user, imagePreviewUrl, updateField }: Previe
   var myDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day), parseInt(hours), parseInt(minutes));
 
   return (
-    <div className="md:grid md:grid-cols-3 gap-6 mt-20 items-start mb-8">
-      <div className="col-span-1 mt-6">
-        <div className="text-xl lg:text-xl font-semibold mb-2 border-b-2 border-gray-300 pb-1">Name</div>
-        <p className="text-m">{form.name}</p>
+    <div className="lg:grid lg:grid-cols-3 mt-20 space-y-6">
+      <div className="md:grid md:grid-cols-2 gap-6 mt-0 items-start mb-8 md:col-span-2 space-y-6">
+        <div className="col-span-1 mt-6 mx-2 space-y-6">
+          <div>
+            <div className="text-lg lg:text-lg font-bold mb-2 border-b-2 border-gray-300 text-gray-600">Name</div>
+            <p className="text-m">{form.name}</p>
+          </div>
+          <div>
+            <div className="text-lg lg:text-lg font-bold mb-2 border-b-2 border-gray-300 text-gray-600">Location</div>
+            <p className="text-m">{form.location}</p>
+          </div>
+          <div>
+            <div className="text-lg lg:text-lg font-bold mb-2 border-b-2 border-gray-300 text-gray-600">
+              Details of Time
+            </div>
+            <div className="flex justify-between">
+              <p className="text-m">Time Start: </p>
+              <p className="text-m">{form.startTime}</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-m">Time End: </p>
+              <p className="text-m">{form.endTime} </p>
+            </div>
+          </div>
+        </div>
 
-        <div className="text-xl lg:text-xl font-semibold mt-6 mb-2 border-b-2 border-gray-300 pb-1">Location</div>
-        <p className="text-m">{form.location}</p>
+        <div className="col-span-1 mt-6 mx-2 space-y-6">
+          <div>
+            <div className="text-lg lg:text-lg font-bold mb-2 border-b-2 border-gray-300 text-gray-600">Sport</div>
+            <p className="text-m">{form.sport}</p>
+          </div>
 
-        <div className="text-xl lg:text-xl font-semibold mt-6 mb-2 border-b-2 border-gray-300 pb-1">
-          Details of Time
+          <div>
+            <div className="text-lg lg:text-lg font-bold mb-2 border-b-2 border-gray-300 text-gray-600">Price</div>
+            <p className="text-m">{form.price}</p>
+          </div>
+
+          <div>
+            <div className="text-lg lg:text-lg font-bold mb-2 border-b-2 border-gray-300 text-gray-600">Capacity</div>
+            <p className="text-m">{form.capacity}</p>
+          </div>
         </div>
-        <div className="flex justify-between">
-          <p className="text-m">Time Start: </p>
-          <p className="text-m">{form.startTime}</p>
+
+        <div className="col-span-2 mx-2">
+          <div className="text-lg lg:text-lg font-bold border-b-2 border-gray-300 pb-1 text-gray-600">Description</div>
+          <div className="text-m" dangerouslySetInnerHTML={{ __html: form.description }}></div>
         </div>
-        <div className="flex justify-between">
-          <p className="text-m">Time End: </p>
-          <p className="text-m">{form.endTime} </p>
-        </div>
-        <div className="text-xl lg:text-xl font-semibold mt-6 mb-2 border-b-2 border-gray-300 pb-1">Sport</div>
-        <p className="text-m">{form.sport}</p>
       </div>
-
-      <div className="col-span-1 mt-6">
-        <div className="text-xl lg:text-xl font-semibold mb-2 border-b-2 border-gray-300 pb-1">Price</div>
-        <p className="text-m">{form.price}</p>
-
-        <div className="text-xl lg:text-xl font-semibold mt-6 mb-2 border-b-2 border-gray-300 pb-1">Capacity</div>
-        <p className="text-m">{form.capacity}</p>
-
-        <div className="text-xl lg:text-xl font-semibold mt-6 mb-2 border-b-2 border-gray-300 pb-1">Description</div>
-        <div className="text-m" dangerouslySetInnerHTML={{ __html: form.description }}></div>
-      </div>
-
-      <div className="col-span-1 mt-6 mx-10">
-        <EventCard
-          eventId=""
-          image={
-            imagePreviewUrl === ""
-              ? "https://firebasestorage.googleapis.com/v0/b/socialsports-44162.appspot.com/o/users%2Fgeneric%2Fgeneric-sports.jpeg?alt=media&token=045e6ecd-8ca7-4c18-a136-71e4aab7aaa5"
-              : imagePreviewUrl
-          }
-          name={form.name}
-          organiser={user}
-          startTime={Timestamp.fromDate(myDate)}
-          location={form.location}
-          price={form.price}
-          vacancy={form.capacity}
-        />
+      <div className="mx-2 col-span-1 flex justify-center lg:justify-end">
+        <div>
+          <div className="text-lg lg:text-lg font-bold mb-2 text-gray-600 text-center">Your post preview:</div>
+          <EventCard
+            eventId=""
+            image={
+              imagePreviewUrl === ""
+                ? "https://firebasestorage.googleapis.com/v0/b/socialsports-44162.appspot.com/o/users%2Fgeneric%2Fgeneric-sports.jpeg?alt=media&token=045e6ecd-8ca7-4c18-a136-71e4aab7aaa5"
+                : imagePreviewUrl
+            }
+            name={form.name}
+            organiser={user}
+            startTime={Timestamp.fromDate(myDate)}
+            location={form.location}
+            price={form.price}
+            vacancy={form.capacity}
+          />
+        </div>
       </div>
     </div>
   );
