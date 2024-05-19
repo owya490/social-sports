671fb65a62a9230d8273fb7fd4d151ebd8cb64d5
Owen <owen.yang@sowaustralia.com>
2024-05-20 02:41:59 +1000

    remove comment

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { isLoggedIn } from "./services/src/auth/authService";
 
 export function middleware(request: NextRequest) {
-  // const { user } = useUser();
   if (!isLoggedIn()) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
