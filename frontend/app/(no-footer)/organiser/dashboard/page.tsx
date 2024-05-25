e56f2798071fe66b38f82a36433548a619ba705f
Edwin <e.ni@student.unsw.edu.au>
2024-05-26 04:04:31 +1000

    Finished for PR

--- frontend/app/(no-footer)/organiser/dashboard/page.tsx
+++ frontend/app/(no-footer)/organiser/dashboard/page.tsx
@@ -10,31 +10,30 @@ import { useUser } from "@/components/utility/UserContext";
 import OrganiserCheckbox from "@/components/organiser/OrganiserCheckbox";
 import LoadingSkeletonOrganiserName from "@/components/loading/LoadingSkeletonOrganiserName";
 
-const initialChecklist = [
+interface ChecklistItem {
+  id: number;
+  label: string;
+  link: string;
+  checked: boolean;
+}
+
+const initialChecklist: ChecklistItem[] = [
   { id: 0, checked: false, label: "Add a picture", link: "/profile" },
   { id: 1, checked: false, label: "Add a description", link: "/profile" },
   { id: 2, checked: false, label: "Add a Stripe Account", link: "/event/create" },
-  { id: 3, checked: false, label: "Create your first event!", link: "/event/create" },
+  { id: 3, checked: false, label: "Create your first event", link: "/event/create" },
 ];
+
 export default function Dashboard() {
   const { user } = useUser();
   const [loading, setLoading] = useState(true);
   const [eventDataList, setEventDataList] = useState<EventData[]>([EmptyEventData, EmptyEventData]);
-  const [checklist, setChecklist] = useState(() => {
-    if (typeof window !== "undefined") {
-      const temp = localStorage.getItem("checklist");
-      if (temp == null) {
-        return initialChecklist;
-      } else {
-        const checklist = JSON.parse(temp);
-        return checklist;
-      }
-    }
-  });
+  const [checklist, setChecklist] = useState<ChecklistItem[]>(initialChecklist);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     const fetchEvents = async () => {
-      if (user.userId == "") {
+      if (user.userId === "") {
         return;
       }
       try {
@@ -52,20 +51,46 @@ export default function Dashboard() {
   }, [user]);
 
   useEffect(() => {
-    localStorage.setItem("checklist", JSON.stringify(checklist));
-  }, [checklist]);
+    if (typeof window !== "undefined") {
+      const savedChecklist = localStorage.getItem("checklist");
+      if (savedChecklist) {
+        setChecklist(JSON.parse(savedChecklist));
+      }
+      setIsMounted(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      localStorage.setItem("checklist", JSON.stringify(checklist));
+    }
+    checklist.forEach((item) => {
+      if (item.checked === false) {
+        return;
+      }
+    });
+  }, [checklist, isMounted]);
 
   const handleCheck = (idx: number) => {
     setChecklist(
-      checklist.map((check: { id: number; checked: any }) => {
-        if (check.id == idx) {
+      checklist.map((check) => {
+        if (check.id === idx) {
           return { ...check, checked: !check.checked };
-        } else {
-          return check;
         }
+        return check;
       })
     );
   };
+
+  const resetChecklist = () => {
+    setChecklist(
+      checklist.map((check) => {
+        return { ...check, checked: false };
+      })
+    );
+  };
+
+  const allItemsChecked = checklist.every((item) => item.checked);
 
   return (
     <div className="pt-16 pl-14 max-h-screen">
@@ -79,24 +104,42 @@ export default function Dashboard() {
             <h1 className="pt-4 text-4xl font-semibold text-[#BABABA]">Welcome {user.firstName}</h1>
           )}
           <div className="flex w-full mt-8 max-h-[60vh]">
-            <div className="grow mr-8 flex flex-col">
-              <div className="bg-organiser-light-gray p-8 rounded-2xl">
-                <h1 className="text-2xl font-bold">Finish setting up</h1>
-                {checklist.map((checkbox: { id: number; label: string; link: string; checked: boolean }) => (
-                  <OrganiserCheckbox
-                    key={checkbox.id}
-                    label={checkbox.label}
-                    link={checkbox.link}
-                    checked={checkbox.checked}
-                    onChange={() => handleCheck(checkbox.id)}
-                  />
-                ))}
+            <div className="grow mr-8 flex flex-col w-[40rem]">
+              <div className="bg-organiser-light-gray p-8 rounded-2xl ">
+                {!allItemsChecked && (
+                  <>
+                    <h1 className="text-2xl font-bold">Finish setting up</h1>
+                    {checklist.map((checkbox) => (
+                      <OrganiserCheckbox
+                        key={checkbox.id}
+                        label={checkbox.label}
+                        link={checkbox.link}
+                        checked={checkbox.checked}
+                        onChange={() => handleCheck(checkbox.id)}
+                      />
+                    ))}
+                  </>
+                )}
+                {allItemsChecked && (
+                  <>
+                    <h1 className="text-center py-16 font-bold text-2xl">
+                      Good job you have finished setting up ✅ <br></br>
+                      Go out there and make more events
+                    </h1>
+                    <p
+                      className="text-[#BABABA] text-end hover:underline hover:cursor-pointer"
+                      onClick={resetChecklist}
+                    >
+                      Reset
+                    </p>
+                  </>
+                )}
               </div>
-              <div className="flex mt-8 grow">
+              <div className="flex mt-8 grow min-h-[10vh]">
                 <div className="flex-1 min-h-full font-semibold text-2xl bg-organiser-light-gray mr-8 rounded-2xl hover:bg-highlight-yellow hover:text-white hover:cursor-pointer">
-                  <div className="h-full flex text-center items-center">
+                  <div className="h-full flex justify-center items-center">
                     <Link href="/event/create">
-                      <p>Create an event template</p>
+                      <p>Create an event</p>
                     </Link>
                   </div>
                 </div>
@@ -144,7 +187,7 @@ export default function Dashboard() {
                       </div>
                     );
                   })}
-                {eventDataList.length == 0 && (
+                {eventDataList.length === 0 && (
                   <div className="bg-organiser-light-gray p-4 rounded-2xl mt-8 min-h-full h-full">
                     <h1 className="text-2xl font-normal text-center">No Events 😔</h1>
                   </div>
