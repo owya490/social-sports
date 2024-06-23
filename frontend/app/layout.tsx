b0fb56587badece5ffabe63def152b3a1093cbc1
Owen <owen.yang@sowaustralia.com>
2024-06-24 02:45:22 +1000

    added favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -12,6 +12,18 @@ export const metadata: Metadata = {
   title: "SportsHub | Book your next social sports session",
   description:
     "SportsHub is a modern, not for profit platform for you to find, book and host your next social sports session. We make it easy for players to search for and book their sport session of choice and for organisers to seamlessly host their next session, with integrated booking and management systems. Try it out free today!",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "favicons/favicon-32x32-black.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/favicons/favicon-32x32-white.png",
+      },
+    ],
+  },
 };
 
 const roboto_condensed = Roboto_Condensed({
