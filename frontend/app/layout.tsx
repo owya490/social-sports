0582146dd5a90a74f0f7053c342662db9fcb0b1d
Owen <owen.yang@sowaustralia.com>
2024-06-24 21:20:43 +1000

    update to caps SPORTSHUB

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -9,10 +9,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SportsHub | Book your next social sports session",
+  title: "SPORTSHUB | Book your next social sports session",
   description:
-    "SportsHub is a modern, not for profit platform for you to find, book and host your next social sports session. We make it easy for players to search for and book their sport session of choice and for organisers to seamlessly host their next session, with integrated booking and management systems. Try it out free today!",
-  icons: {
+    "SPORTSHUB is a modern, not for profit platform for you to find, book and host your next social sports session. We make it easy for players to search for and book their sport session of choice and for organisers to seamlessly host their next session, with integrated booking and management systems. Try it out free today!",
+  icons: {  
     icon: [
       {
         media: "(prefers-color-scheme: light)",
