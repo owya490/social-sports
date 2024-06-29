32ab738026efe76b59f04373c26ecd7dca69f840
Brian Wang <bwang3@snapchat.com>
2024-06-30 01:26:02 +1000

    changed users refresh millis to 10 minutes

--- frontend/services/src/users/usersConstants.ts
+++ frontend/services/src/users/usersConstants.ts
@@ -1,4 +1,4 @@
-export const USERS_REFRESH_MILLIS = 5 * 60 * 1000; // Millis of 5 Minutes
+export const USERS_REFRESH_MILLIS = 10 * 60 * 1000; // Millis of 10 Minutes
 
 export enum UsersLocalStorageKeys {
   UsersData = "usersData",
