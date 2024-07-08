fb733e8833b9a22c3dfec1c7c120d911f17a1411
Edwin <e.ni@student.unsw.edu.au>
2024-07-08 15:39:51 +1000

    Changed Logo

--- frontend/components/navbar/Logo.tsx
+++ frontend/components/navbar/Logo.tsx
@@ -1,18 +1,11 @@
 import Image from "next/image";
-import LogoImage from "./../../public/images/SportsHubLogo.png";
+import LogoImage from "./../../public/images/SportsHubBlackLogo.svg";
 import Link from "next/link";
 
 export default function Logo() {
   return (
     <Link href="/dashboard" className="flex items-center">
-      <Image
-        priority={true}
-        src={LogoImage}
-        alt="Logo"
-        width={0}
-        height={0}
-        className="w-80 mr-8"
-      />
+      <Image priority={true} src={LogoImage} alt="Logo" width={0} height={0} className="w-60 h-10" />
     </Link>
   );
 }
