29c53abdd3871c3c71c3377296d7f932907b4753
Aidan Chee <aidanyschee@gmail.com>
2024-07-15 18:18:40 +1000

    functional organiser navabr

---
+++ frontend/app/(no-footer)/organiser/gallery/page.tsx
@@ -0,0 +1,12 @@
+//gallery
+"use client";
+import OrganiserNavbar from "@/components/organiser/OrganiserNavbar";
+
+export default function Gallery() {
+  return (
+    <div className="pt-16 pl-14 max-h-screen">
+      <OrganiserNavbar currPage="Gallery" />
+      <div className="pt-16 pl-14">hello im coming soon</div>
+    </div>
+  );
+}
