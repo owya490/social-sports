ca09da177e29e635411eacd577457c2ec844f91f
Aidan Chee <aidanyschee@gmail.com>
2024-07-15 21:30:39 +1000

    navbar structure

--- frontend/components/navbar/ProfilePic.tsx
+++ frontend/components/navbar/ProfilePic.tsx
@@ -4,7 +4,6 @@ import { sleep } from "@/utilities/sleepUtil";
 import { Menu, MenuButton, MenuItems, Transition } from "@headlessui/react";
 import {
   ArrowLeftStartOnRectangleIcon,
-  Cog8ToothIcon,
   LifebuoyIcon,
   LightBulbIcon,
   UserCircleIcon,
@@ -133,50 +132,37 @@ export default function ProfilePic() {
               leaveTo="transform opacity-0 scale-95"
             >
               <MenuItems className="absolute right-0 mt-1 w-52 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                {loggedIn && (
-                  <div className="px-1 py-1">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          href="/profile"
-                          className={`${
-                            active ? "text-white bg-highlight-yellow" : "text-black"
-                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                        >
-                          <UserCircleIcon className="h-5 mr-2" />
-                          Profile
-                        </Link>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <Link
-                          href="/settings"
-                          className={`${
-                            active ? "text-white bg-highlight-yellow" : "text-black"
-                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                        >
-                          <Cog8ToothIcon className="h-5 mr-2" />
-                          Settings
-                        </Link>
-                      )}
-                    </Menu.Item>
-                  </div>
-                )}
                 <div className="px-1 py-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/organiser/dashboard"
-                        className={`${
-                          active ? "text-white bg-highlight-yellow" : "text-black"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <LifebuoyIcon className="h-5 mr-2" />
-                        Organiser Hub
-                      </Link>
-                    )}
-                  </Menu.Item>
+                  {loggedIn && (
+                    <div>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="/profile"
+                            className={`${
+                              active ? "text-white bg-highlight-yellow" : "text-black"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            <UserCircleIcon className="h-5 mr-2" />
+                            Profile
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="/organiser/dashboard"
+                            className={`${
+                              active ? "text-white bg-highlight-yellow" : "text-black"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            <LifebuoyIcon className="h-5 mr-2" />
+                            Organiser Hub
+                          </Link>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  )}
                   <Menu.Item>
                     {({ active }) => (
                       <Link
@@ -191,8 +177,8 @@ export default function ProfilePic() {
                     )}
                   </Menu.Item>
                 </div>
-                {loggedIn && (
-                  <div className="px-1 py-1">
+                <div className="px-1 py-1">
+                  {loggedIn && (
                     <Menu.Item>
                       {({ active }) => (
                         <div
@@ -206,8 +192,8 @@ export default function ProfilePic() {
                         </div>
                       )}
                     </Menu.Item>
-                  </div>
-                )}
+                  )}
+                </div>
               </MenuItems>
             </Transition>
           </Menu>
