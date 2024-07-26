192a521186d9a75ed77f884c01ca1ac20b933950
inzaenity <118352275+inzaenity@users.noreply.github.com>
2024-07-26 15:13:30 +1000

    fixing merge

--- frontend/components/utility/UserContext.tsx
+++ frontend/components/utility/UserContext.tsx
@@ -5,7 +5,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { EmptyUserData, UserData } from "../../interfaces/UserTypes";
 import { getFullUserByIdForUserContextWithRetries } from "../../services/src/users/usersService";
 
-import { redirect, usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 type LoginUserContextType = {
   user: UserData;
@@ -20,9 +20,7 @@ export const LoginUserContext = createContext<LoginUserContextType>({
 export default function UserContext({ children }: { children: any }) {
   const [user, setUser] = useState<UserData>(EmptyUserData);
   const router = useRouter();
-  const pathname = usePathname();
 
-  const protectedRoutes = ["/organiser"];
   useEffect(() => {
     const unsubscriber = onAuthStateChanged(auth, async (userAuth) => {
       if (userAuth && auth.currentUser?.emailVerified) {
@@ -33,24 +31,10 @@ export default function UserContext({ children }: { children: any }) {
         } catch {
           router.push("/error");
         }
-      } else {
-        router.replace("/login");
       }
     });
-
     return () => unsubscriber();
   }, []);
-
-  useEffect(() => {
-    console.log("Checking authentication and protected routes");
-    console.log("Current user:", user);
-    console.log("Current pathname:", pathname);
-
-    if (user === EmptyUserData && protectedRoutes.some((prefix) => pathname.startsWith(prefix))) {
-      console.log("why is this being triggered", user);
-      router.push("/login");
-    }
-  }, [user, pathname]);
 
   return <LoginUserContext.Provider value={{ user, setUser }}>{children}</LoginUserContext.Provider>;
 }
