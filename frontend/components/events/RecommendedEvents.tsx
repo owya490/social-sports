456652db4b9cf1975d5a9a1140f042c38f19ba72
Owen <owen.yang@sowaustralia.com>
2024-08-08 20:41:54 +1000

    hot fixes before demo

--- frontend/components/events/RecommendedEvents.tsx
+++ frontend/components/events/RecommendedEvents.tsx
@@ -79,7 +79,7 @@ export default function RecommendedEvents(props: RecommendedEventsProps) {
             <div className="w-full bg-gray-300 h-[1px] mt-10"></div>
             <div className="flex my-5">
               <h5 className="font-bold text-lg">Similar events nearby</h5>
-              <Link href="#" className="text-sm font-light ml-auto cursor-pointer hover:underline">
+              <Link href="/dashboard" className="text-sm font-light ml-auto cursor-pointer hover:underline">
                 See all
               </Link>
             </div>
