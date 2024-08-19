b614ab69d2414e32f5798d3e040f2b8e2157bbe1
Owen <owen.yang@sowaustralia.com>
2024-08-19 20:55:39 +1000

    address comments

--- frontend/utilities/priceUtils.ts
+++ frontend/utilities/priceUtils.ts
@@ -6,6 +6,19 @@ export function centsToDollars(cents: number): number {
   return parseFloat((cents / 100).toFixed(2));
 }
 
+
+/**
+ * This function is used extensively throughout our code base to display pricing in a human readable format.
+ * Namely currently dollars and cents to 2 decimal places.
+ * 
+ * As the price is stored purely as cents in our database, we need to convert it to dollars and cents for humans
+ * to read on the frontend.
+ * 
+ * E.g. price of 1527 cents in the database will be displayed as $15.27 on the frontend.
+ * 
+ * @param price price of the item from the database, which will be in cents
+ * @returns returns a number which represents the display format of the price
+ */
 export function displayPrice(price: number): number {
   // The price in the datastore and in our memory is all cents
   return centsToDollars(price);
