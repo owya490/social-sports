863cfbf06aba9c225009ba5cf5346420e3ef68ff
Owen <owen.yang@sowaustralia.com>
2024-08-25 22:26:29 +1000

    add key to nav tab

--- frontend/components/organiser/mobile/MobileEventDrilldownNavTabs.tsx
+++ frontend/components/organiser/mobile/MobileEventDrilldownNavTabs.tsx
@@ -8,9 +8,10 @@ export const MobileEventDrilldownNavTabs = ({ currSidebarPage, setCurrSidebarPag
 
   return (
     <div className="flex sm:hidden">
-      {navigationTabs.map((tab) => {
+      {navigationTabs.map((tab, idx) => {
         return (
           <button
+            key={`navTab${idx}`}
             className={`px-2 py-2 text-center overflow-hidden text-xs basis-1/2 ${
               currSidebarPage === tab ? "border-b-[3px] border-black font-bold" : ""
             }`}
