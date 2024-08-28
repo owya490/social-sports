2456220fcfe588adc22e47eac7f23f79e5585236
Edwin <e.ni@student.unsw.edu.au>
2024-08-28 14:12:08 +1000

    Fixing padding/margin issues

--- frontend/app/(footer)/dashboard/page.tsx
+++ frontend/app/(footer)/dashboard/page.tsx
@@ -158,7 +158,7 @@ export default function Dashboard() {
           Successfully logged in!
         </Alert>
       </div>
-      <div className="flex justify-center">
+      <div className="flex justify-center mt-4">
         <div className="pb-10 screen-width-dashboard">
           {loading === false && eventDataList.length === 0 && (
             <div className="flex justify-center z-10">
@@ -176,7 +176,7 @@ export default function Dashboard() {
               </div>
             </div>
           )}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 justify-items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-8 justify-items-center">
             {eventDataList
               .sort((event1, event2) => {
                 if (event1.accessCount > event2.accessCount) {
@@ -189,7 +189,7 @@ export default function Dashboard() {
               })
               .map((event, eventIdx) => {
                 return (
-                  <div className="my-4 w-full" key={eventIdx}>
+                  <div className="w-full" key={eventIdx}>
                     <EventCard
                       eventId={event.eventId}
                       image={event.image}
