ac6fb0ecccc4d037d6d27013662f36849daf6a74
Edwin <e.ni@student.unsw.edu.au>
2024-08-28 13:53:14 +1000

    Fixing loading skeleton

--- frontend/components/loading/LoadingSkeletonOrganiserEventCard.tsx
+++ frontend/components/loading/LoadingSkeletonOrganiserEventCard.tsx
@@ -4,37 +4,37 @@ export default function LoadingSkeletonOrganiserEventCard() {
   return (
     <div className="w-full px-2">
       <Skeleton
-        height={140}
+        height={150}
         wrapper={({ children }) => {
-          return <div className="h-44 flex items-center w-full">{children}</div>;
+          return <div className="h-[10.5rem] flex items-center w-full">{children}</div>;
         }}
       />
       <Skeleton
         height={6}
         width={190}
         wrapper={({ children }) => {
-          return <div className="h-3 flex items-center">{children}</div>;
+          return <div className="h-3 flex items-center ml-2">{children}</div>;
         }}
       />
       <Skeleton
         height={20}
         width={240}
         wrapper={({ children }) => {
-          return <div className="h-8 flex items-center">{children}</div>;
+          return <div className="h-8 flex items-center ml-2">{children}</div>;
         }}
       />
       <Skeleton
         height={6}
         width={160}
         wrapper={({ children }) => {
-          return <div className="h-5 flex items-center ml-2 mt-5">{children}</div>;
+          return <div className="h-5 flex items-center ml-2 mt-4">{children}</div>;
         }}
       />
       <Skeleton
         height={6}
         width={160}
         wrapper={({ children }) => {
-          return <div className="h-5 flex items-center ml-2">{children}</div>;
+          return <div className="h-5 flex items-center ml-2 mt-1">{children}</div>;
         }}
       />
 
@@ -42,7 +42,7 @@ export default function LoadingSkeletonOrganiserEventCard() {
         height={6}
         width={100}
         wrapper={({ children }) => {
-          return <div className="h-3 flex items-center mt-10 mb-4">{children}</div>;
+          return <div className="h-3 flex items-center mt-10 mb-4 ml-2">{children}</div>;
         }}
       />
     </div>
