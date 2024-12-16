9f5e1e401f369195208bcf861f2cd1f23bef31ea
Aidan Chee <aidanyschee@gmail.com>
2024-12-16 20:56:54 +1100

    white dark

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,6 @@ import MobileNavbar from "@/components/mobile/MobileNavbar";
 import Navbar from "@/components/navbar/Navbar";
 import UserContext from "@/components/utility/UserContext";
 import GrafanaFaro from "@/observability/GrafanaFaro";
-import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -33,29 +32,30 @@ const aileron = localFont({
 
 // const inter = Inter({ subsets: ["latin"] }); old font, just replace aileron with inter to swap back
 
-export const metadata: Metadata = {
-  title: "SportsHub | Book your next sports session",
-  description:
-    "SportsHub is a modern, not for profit platform for you to find, book and host your next social sports session. We make it easy for players to search for and book their sport session of choice and for organisers to seamlessly host their next session, with integrated booking and management systems. Try it out free today!",
-  icons: {
-    icon: [
-      {
-        media: "(prefers-color-scheme: light)",
-        url: "favicons/favicon-32x32-black.png",
-      },
-      {
-        media: "(prefers-color-scheme: dark)",
-        url: "/favicons/favicon-32x32-white.png",
-      },
-    ],
-  },
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <title>SportsHub | Book your next sports session</title>
+        <meta
+          name="description"
+          content="SportsHub is a modern, not-for-profit platform for you to find, book, and host your next social sports session. We make it easy for players to search and book their sport session of choice and for organizers to seamlessly host their next session with integrated booking and management systems. Try it out free today!"
+        />
         <link rel="icon" type="image/x-icon" href="/favicons/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32-black.png"
+          media="(prefers-color-scheme: light)"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32-white.png"
+          media="(prefers-color-scheme: dark)"
+        />
         <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
         <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
