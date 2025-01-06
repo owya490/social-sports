6d8185a30ae2aebd6f5858ce0045d95e4e2b07ab
Aidan Chee <aidanyschee@gmail.com>
2025-01-06 15:34:47 +1100

    fuck me

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import MobileNavbar from "@/components/mobile/MobileNavbar";
 import Navbar from "@/components/navbar/Navbar";
 import UserContext from "@/components/utility/UserContext";
 import GrafanaFaro from "@/observability/GrafanaFaro";
+import { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -35,35 +36,6 @@ const aileron = localFont({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <title>SportsHub | Book your next sports session</title>
-        <meta
-          name="description"
-          content="SportsHub is a modern, not-for-profit platform for you to find, book, and host your next social sports session. We make it easy for players to search and book their sport session of choice and for organizers to seamlessly host their next session with integrated booking and management systems. Try it out free today!"
-        />
-        <link rel="icon" type="image/x-icon" href="/favicons/favicon.ico" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32-black.png"
-          media="(prefers-color-scheme: light)"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32-white.png"
-          media="(prefers-color-scheme: dark)"
-        />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
-        <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
-        <meta name="apple-mobile-web-app-title" content="SportsHub" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-      </head>
-
       <GrafanaFaro>
         <UserContext>
           <body className={`${aileron.className}`}>
@@ -80,3 +52,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  title: "Sports Hub",
+  description: "test",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/images/icon-light.png",
+        href: "/images/icon-light.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/images/icon-dark.png",
+        href: "/images/icon-dark.png",
+      },
+    ],
+  },
+};
