25aeba824fada42144df815aa3c8ce9b14888622
Brian Wang <brian.wang900@gmail.com>
2025-02-16 14:33:05 +1100

    SPORTSHUB-236 established data types for forms and stubbed out functions

---
+++ frontend/services/src/forms/formsServices.ts
@@ -0,0 +1,59 @@
+import { Form, FormId, FormResponse, FormSection, FormResponseId, SectionId } from "@/interfaces/FormTypes";
+import { UserId } from "@/interfaces/UserTypes";
+
+export async function createForm(form: Form): Promise<void> {
+  // TODO:
+}
+
+export async function getForm(formId: FormId): Promise<Form> {
+  // TODO:
+  return {
+    title: "",
+    sectionsOrder: [],
+    sectionsMap: new Map<SectionId, FormSection>(),
+  };
+}
+
+export async function editForm(formData: Partial<Form>, formId: FormId): Promise<void> {
+  // TODO:
+}
+
+export async function archiveForm(formId: FormId): Promise<void> {
+  // TODO:
+}
+
+export async function deleteForm(formId: FormId): Promise<void> {
+  // TODO:
+}
+
+export async function createFormResponse(formResponse: FormResponse): Promise<FormResponseId> {
+  // TODO:
+  return "";
+}
+
+export async function getFormResponse(
+  formId: FormId,
+  userId: UserId,
+  responseId: FormResponseId
+): Promise<FormResponse> {
+  // TODO:
+  return {
+    formId: "",
+    userId: "",
+    responseMap: new Map<SectionId, FormSection>(),
+    submissionTime: Date.now(),
+  };
+}
+
+export async function editFormResponse(
+  formId: FormId,
+  userId: UserId,
+  responseId: FormResponseId,
+  formResponse: FormResponse
+): Promise<void> {
+  // TODO:
+}
+
+export async function deleteFormResponse(formId: FormId, userId: UserId, responseId: FormResponseId): Promise<void> {
+  // TODO:
+}
