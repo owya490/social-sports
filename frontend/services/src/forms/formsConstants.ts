a00bf9267641c94ddf23970c31e1f4ba9c78a734
Brian Wang <brian.wang900@gmail.com>
2025-02-21 09:04:57 +1100

    SPORTSHUB-339 implemented createForm and getForm functions

--- frontend/services/src/forms/formsConstants.ts
+++ frontend/services/src/forms/formsConstants.ts
@@ -1,5 +1,7 @@
 export const FORMS_REFRESH_MILLIS = 5 * 60 * 1000; // Millis of 5 minutes
 
+export const FORMS_MAX_EVENTS = 5;
+
 export enum CollectionPaths {
   Forms = "Forms",
 }
@@ -7,4 +9,14 @@ export enum CollectionPaths {
 export enum FormStatus {
   Active = "Active",
   Deleted = "Deleted",
+}
+
+export enum FormPaths {
+  FormsActive = "Forms/Active",
+  FormsDeleted = "Forms/Deleted",
+}
+
+export enum LocalStorageKeys {
+  FormsOperationCount5Min = "formsOperationCount5Min",
+  FormsLastCreateUpdateOperationTimestamp = "formsLastCreateUpdateOperationTimestamp",
 }
--- frontend/services/src/forms/formsServices.ts
+++ frontend/services/src/forms/formsServices.ts
@@ -9,34 +9,52 @@ import {
 } from "@/interfaces/FormTypes";
 import { UserId } from "@/interfaces/UserTypes";
 import { Logger } from "@/observability/logger";
-import { doc, setDoc, writeBatch } from "firebase/firestore";
+import { doc, getDoc, setDoc, writeBatch } from "firebase/firestore";
 import { db } from "../firebase";
-import { CollectionPaths, FormStatus } from "./formsConstants";
+import { CollectionPaths, FormPaths, FormStatus } from "./formsConstants";
+import { rateLimitCreateForm } from "./formsUtils/createFormUtils";
 
 export const formsServiceLogger = new Logger("formsServiceLogger");
 
-export async function createForm(form: Form): Promise<void> {
+export async function createForm(form: Form): Promise<FormId> {
   // TODO: implement rate limiting
+  if (!rateLimitCreateForm()) {
+    formsServiceLogger.warn("Rate Limited!!!");
+  }
   formsServiceLogger.info(`createForm: ${form}`);
-
   try {
     const batch = writeBatch(db);
     const docRef = doc(db, CollectionPaths.Forms, FormStatus.Active);
     batch.set(docRef, form);
     batch.commit();
     formsServiceLogger.info(`Form created successfully: ${form}`);
+
+    return docRef.id;
   } catch (error) {
     formsServiceLogger.error(`createForm: ${error}`);
+    throw error;
   }
 }
 
 export async function getForm(formId: FormId): Promise<Form> {
   // TODO:
-  return {
-    title: "",
-    sectionsOrder: [],
-    sectionsMap: new Map<SectionId, FormSection>(),
-  };
+  try {
+    const formDocRef = doc(db, `${FormPaths.FormsActive}`, formId);
+    const formDoc = await getDoc(formDocRef);
+
+    // Check if the document exists in the current subcollection
+    if (formDoc.exists()) {
+      formsServiceLogger.info(`Found form document reference for formId: ${formId}`);
+      return formDoc.data() as Form;
+    }
+
+    // if no document found, log and throw an error
+    formsServiceLogger.error(`Form document not found for formId: ${formId}`);
+    throw new Error(`Error finding form document with formId: ${formId}`);
+  } catch (error) {
+    formsServiceLogger.error(`Error getting form for formId: ${formId}, error: ${error}`);
+    throw error;
+  }
 }
 
 export async function editForm(formData: Partial<Form>, formId: FormId): Promise<void> {
