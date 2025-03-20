c4c6f5d7b3aedc75e6a384e22708c4221ab2f248
Brian Wang <brian.wang900@gmail.com>
2025-03-20 22:03:28 +1100

    fixed grid

--- frontend/app/(no-footer)/organiser/forms/dashboard/page.tsx
+++ frontend/app/(no-footer)/organiser/forms/dashboard/page.tsx
@@ -1,8 +1,35 @@
+"use client";
+
 import FormsCard from "@/components/organiser/forms/FormsCard";
 import OrganiserNavbar from "@/components/organiser/OrganiserNavbar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const FormsDashboard = () => {
+  const [forms, setForms] = useState<any[]>([]);
+
+  useEffect(() => {
+    setForms([
+      {
+        formid: "test",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/socialsports-44162.appspot.com/o/users%2fbj2l4pt9b1gfjhaocvd9nj1cjfg3%2f1721045243251_screenshot%202024-07-08%20at%201.49.56%e2%80%afpm.png?alt=media&token=7920e1ba-5cd8-461d-8d51-9a43b80543a7",
+        name: "form 1",
+      },
+      {
+        formid: "test2",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/socialsports-44162.appspot.com/o/users%2fbj2l4pt9b1gfjhaocvd9nj1cjfg3%2f1721045243251_screenshot%202024-07-08%20at%201.49.56%e2%80%afpm.png?alt=media&token=7920e1ba-5cd8-461d-8d51-9a43b80543a7",
+        name: "form 1",
+      },
+      {
+        formid: "test2",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/socialsports-44162.appspot.com/o/users%2fbj2l4pt9b1gfjhaocvd9nj1cjfg3%2f1721045243251_screenshot%202024-07-08%20at%201.49.56%e2%80%afpm.png?alt=media&token=7920e1ba-5cd8-461d-8d51-9a43b80543a7",
+        name: "form 1",
+      },
+    ]);
+  }, []);
+
   return (
     <div className="w-screen pt-14 lg:pt-16 lg:pb-10 md:pl-7 h-fit max-h-screen overflow-y-auto">
       <OrganiserNavbar currPage={"FormsDashboard"} />
@@ -11,14 +38,10 @@ const FormsDashboard = () => {
           <div className="flex flex-col items-center justify-center">
             <div className="text-4xl md:text-5xl lg:text-6xl my-6">Forms Dashboard</div>
             <div className="flex flex-row h-full w-full">
-              <div className="z-5 grid grid-cols-1 xl:grid-cols-2 gap-8 justify-items-center px-4 min-w-[300px] lg:min-w-[600px] 2xl:min-w-[960px] 3xl:min-w-[1280px] h-[68vh] lg:h-[80vh]">
-                <div className="w-full">
-                  <FormsCard
-                    formId="test"
-                    image="https://firebasestorage.googleapis.com/v0/b/socialsports-44162.appspot.com/o/users%2FbJ2L4Pt9B1gfjHaoCVD9nj1cjFG3%2F1721045243251_Screenshot%202024-07-08%20at%201.49.56%E2%80%AFPM.png?alt=media&token=7920e1ba-5cd8-461d-8d51-9a43b80543a7"
-                    name="Form 1"
-                  ></FormsCard>
-                </div>
+              <div className="z-5 grid grid-cols-1 xl:grid-cols-2 gap-6 justify-items-center px-4 min-w-[300px] lg:min-w-[640px] 2xl:min-w-[1032px] 3xl:min-w-[1372px] h-auto">
+                {forms.map((form) => (
+                  <FormsCard key={form.formId} formId={form.formId} image={form.image} name={form.name}></FormsCard>
+                ))}
               </div>
             </div>
           </div>
