4abf15a358c1d897bbec6a08df9036b5c6c087fb
Richard Peng <richardpeng914@gmail.com>
2025-04-07 20:40:50 +1000

    this works

--- frontend/app/(no-footer)/organiser/forms/[formId]/form-editor/page.tsx
+++ frontend/app/(no-footer)/organiser/forms/[formId]/form-editor/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+"use client";
 
-const FormEditor = () => {
-  return <div>FormEditor</div>;
+import FormEditor from '@/components/organiser/forms/FormEditor';
+import { useParams } from 'next/navigation';
+
+const FormEditorPage = () => {
+  const params = useParams();
+  const formId = params?.formId;
+
+  console.log('FormEditorPage params:', params); // Debugging line
+  console.log('FormEditorPage formId:', formId); // Debugging line
+
+  return <FormEditor formid={formId as string} />;
 };
 
-export default FormEditor;
+export default FormEditorPage;
