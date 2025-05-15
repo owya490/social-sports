d6ec0232c4127ef9704d0f5b65ab80518f9747d6
Edwin <e.ni@student.unsw.edu.au>
2025-05-16 04:12:29 +1000

    Initial try @ organiser mobile navbar

--- frontend/app/(no-footer)/organiser/layout.tsx
+++ frontend/app/(no-footer)/organiser/layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <>
       <OrganiserNavbar />
-      <div className="sm:ml-14 px-2 pb-20 sm:p-4">{children}</div>
+      <div className="sm:ml-14 px-2 pb-32 sm:p-4">{children}</div>
     </>
   );
 }
--- frontend/components/organiser/OrganiserNavbar.tsx
+++ frontend/components/organiser/OrganiserNavbar.tsx
@@ -19,6 +19,7 @@ import { Timestamp } from "firebase/firestore";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Fragment, ReactNode, useEffect, useState } from "react";
+import OrganiserMobileNavbar from "./mobile/OrganiserMobileNavbar";
 
 interface ResponsiveTooltipProps {
   content: string;
@@ -70,7 +71,6 @@ export default function OrganiserNavbar() {
   const [_closestEvent, setClosestEvent] = useState<EventData | null>(null);
   const [eventId, setEventId] = useState<string>("dashboard");
   const currPage = usePathname();
-  console.log(currPage);
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -110,102 +110,108 @@ export default function OrganiserNavbar() {
   }, [user]);
 
   return (
-    <div className="bg-white border-r-[1px] border-core-outline fixed bottom-0 sm:bottom-auto inset-x-0 sm:inset-x-auto sm:left-0 sm:h-screen z-40">
-      <div className="flex justify-center items-center h-12 sm:h-auto sm:w-14 sm:flex-col sm:mt-14 sm:space-y-3 sm:space-x-0 space-x-3">
-        <NavButton
-          href="/organiser/dashboard/"
-          isActive={currPage.startsWith("/organiser/dashboard")}
-          tooltipContent="Dashboard"
-        >
-          <HomeIcon className="w-6 stroke-1 stroke-core-text" />
-        </NavButton>
+    <>
+      {/* Desktop Sidebar */}
+      <div className="hidden sm:block bg-white border-r-[1px] border-core-outline fixed bottom-0 sm:bottom-auto inset-x-0 sm:inset-x-auto sm:left-0 sm:h-screen z-40">
+        <div className="flex justify-center items-center h-12 sm:h-auto sm:w-14 sm:flex-col sm:mt-14 sm:space-y-3 sm:space-x-0 space-x-3">
+          <NavButton
+            href="/organiser/dashboard/"
+            isActive={currPage.startsWith("/organiser/dashboard")}
+            tooltipContent="Dashboard"
+          >
+            <HomeIcon className="w-6 stroke-1 stroke-core-text" />
+          </NavButton>
 
-        <ResponsiveTooltip content="Events">
-          <Menu as="div" className="relative inline-block text-left">
-            <div className="flex items-centers">
-              <MenuButton
-                className={`flex justify-center items-center self-center h-10 w-10 sm:h-10 sm:w-10 sm:m-auto rounded-md hover:bg-core-hover transition ease-in-out ${
-                  currPage.startsWith("/organiser/event/") && "bg-core-hover"
-                }`}
+          <ResponsiveTooltip content="Events">
+            <Menu as="div" className="relative inline-block text-left">
+              <div className="flex items-centers">
+                <MenuButton
+                  className={`flex justify-center items-center self-center h-10 w-10 sm:h-10 sm:w-10 sm:m-auto rounded-md hover:bg-core-hover transition ease-in-out ${
+                    currPage.startsWith("/organiser/event/") && "bg-core-hover"
+                  }`}
+                >
+                  <CalendarIcon className="w-6 stroke-1 stroke-core-text" />
+                </MenuButton>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
               >
-                <CalendarIcon className="w-6 stroke-1 stroke-core-text" />
-              </MenuButton>
-            </div>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <MenuItems className="absolute bottom-16 left-0 md:left-16 md:top-0 mt-1 w-52 h-fit origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <div className="px-1 py-1 ">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/organiser/event/dashboard"
-                        className={`${
-                          active ? "text-core-text bg-core-hover" : "text-core-text"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <StarIcon className="w-6 stroke-1 mr-2" />
-                        Event Dashboard
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/organiser/recurring-events"
-                        className={`${
-                          active ? "text-core-text bg-core-hover" : "text-core-text"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <ArrowPathIcon className="w-6 stroke-1 mr-2" />
-                        Recurring Events
-                      </Link>
-                    )}
-                  </Menu.Item>
-                </div>
-              </MenuItems>
-            </Transition>
-          </Menu>
-        </ResponsiveTooltip>
+                <MenuItems className="absolute bottom-16 left-0 md:left-16 md:top-0 mt-1 w-52 h-fit origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <div className="px-1 py-1 ">
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href="/organiser/event/dashboard"
+                          className={`${
+                            active ? "text-core-text bg-core-hover" : "text-core-text"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          <StarIcon className="w-6 stroke-1 mr-2" />
+                          Event Dashboard
+                        </Link>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({ active }) => (
+                        <Link
+                          href="/organiser/recurring-events"
+                          className={`${
+                            active ? "text-core-text bg-core-hover" : "text-core-text"
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                        >
+                          <ArrowPathIcon className="w-6 stroke-1 mr-2" />
+                          Recurring Events
+                        </Link>
+                      )}
+                    </Menu.Item>
+                  </div>
+                </MenuItems>
+              </Transition>
+            </Menu>
+          </ResponsiveTooltip>
 
-        <NavButton
-          href={`/organiser/event/${eventId}`}
-          isActive={currPage.startsWith("/organiser/event/")}
-          tooltipContent="Current Event"
-        >
-          <BookmarkSquareIcon className="w-6 stroke-1 stroke-core-text" />
-        </NavButton>
+          <NavButton
+            href={`/organiser/event/${eventId}`}
+            isActive={currPage.startsWith("/organiser/event/")}
+            tooltipContent="Current Event"
+          >
+            <BookmarkSquareIcon className="w-6 stroke-1 stroke-core-text" />
+          </NavButton>
 
-        <NavButton
-          href="/organiser/metrics"
-          isActive={currPage.startsWith("/organiser/metrics")}
-          tooltipContent="Metrics"
-        >
-          <ChartBarIcon className="w-6 stroke-1 stroke-core-text" />
-        </NavButton>
+          <NavButton
+            href="/organiser/metrics"
+            isActive={currPage.startsWith("/organiser/metrics")}
+            tooltipContent="Metrics"
+          >
+            <ChartBarIcon className="w-6 stroke-1 stroke-core-text" />
+          </NavButton>
 
-        <NavButton
-          href="/organiser/gallery"
-          isActive={currPage.startsWith("/organiser/gallery")}
-          tooltipContent="Gallery"
-        >
-          <CameraIcon className="w-6 stroke-1 stroke-core-text" />
-        </NavButton>
+          <NavButton
+            href="/organiser/gallery"
+            isActive={currPage.startsWith("/organiser/gallery")}
+            tooltipContent="Gallery"
+          >
+            <CameraIcon className="w-6 stroke-1 stroke-core-text" />
+          </NavButton>
 
-        <NavButton
-          href="/organiser/settings"
-          isActive={currPage.startsWith("/organiser/settings")}
-          tooltipContent="Settings"
-        >
-          <UserIcon className="w-6 stroke-1 stroke-core-text" />
-        </NavButton>
+          <NavButton
+            href="/organiser/settings"
+            isActive={currPage.startsWith("/organiser/settings")}
+            tooltipContent="Settings"
+          >
+            <UserIcon className="w-6 stroke-1 stroke-core-text" />
+          </NavButton>
+        </div>
       </div>
-    </div>
+
+      {/* Mobile Navigation */}
+      <OrganiserMobileNavbar />
+    </>
   );
 }
