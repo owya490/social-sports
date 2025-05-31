6ee7e98ce9a2e31c32929217604fac809c4cb9f4
Owen <owen.yang@sowaustralia.com>
2025-05-31 18:30:50 +1000

    wip animate navbar

--- frontend/components/navbar/SearchBar.tsx
+++ frontend/components/navbar/SearchBar.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { SearchIcon } from "@/svgs/SearchIcon";
+import { Select } from "@headlessui/react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function SearchBar() {
   const [event, setEvent] = useState("");
@@ -45,16 +46,62 @@ export default function SearchBar() {
       router.push(searchUrl);
     }
   };
+
+  const [searchTypeSelected, setSearchTypeSelected] = useState("events");
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const groupRef = useRef(null);
+  const selectRef = useRef(null);
+
+  const [selectTargetWidth, setSelectTargetWidth] = useState(0);
+
+  useEffect(() => {
+    if (selectRef.current && isFocused) {
+      // @ts-ignore
+      const width = selectRef.current.getBoundingClientRect().width;
+      setSelectTargetWidth(width);
+    } else if (!isFocused) {
+      setSelectTargetWidth(0);
+    }
+  }, [isFocused]);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = (e: any) => {
+    // Check if the newly focused element is still inside the group
+    // @ts-ignore
+    if (groupRef.current && !groupRef.current.contains(e.relatedTarget)) {
+      setIsFocused(false);
+    }
+  };
+
   return (
-    <div className="flex border border-1 border-core-outline rounded-full h-10 pl-5 pr-1 w-fit items-center bg-white shadow-sm">
+    <div
+      className={`flex border border-1 border-core-outline rounded-full h-10 pl-5 pr-1 w-fit items-center shadow-sm group transition-all duration-[400ms] ease-in-out ${
+        isFocused ? "" : `${isHovered ? "bg-core-hover" : ""}`
+      }`}
+      ref={groupRef}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      onMouseLeave={() => {
+        setIsHovered(false);
+      }}
+      onMouseEnter={() => {
+        setIsHovered(true);
+        console.log("hovered");
+      }}
+    >
       <input
-        // className="h-9 max-w-[160px] xl:max-w-[220px] border-0 focus:ring-0"
-        className="h-9 w-64 border-0 focus:ring-0"
+        className={`h-9 w-64 border-0 focus:ring-0 font-thin ${isFocused ? "" : `${isHovered ? "bg-core-hover" : ""}`}`}
         type="text"
         placeholder="Search for events"
         value={event}
         onChange={(e) => setEvent(e.target.value)}
         onKeyDown={handleKeyPress}
+        onMouseEnter={() => setIsHovered(true)}
       />
       {/* <div className="h-full bg-black w-[1px] mx-2"></div> */}
       {/* <input
@@ -65,7 +112,33 @@ export default function SearchBar() {
         onChange={(e) => setLocation(e.target.value)}
         onKeyDown={handleKeyPress}
       /> */}
-      <button onClick={handleSearchClick} className="w-7 h-7 rounded-full border border-black bg-black">
+      {/* {isFocused && ( */}
+      <div
+        className={`overflow-hidden transition-all duration-[400ms] mr-1 `}
+        style={{ width: `${selectTargetWidth}px` }}
+      >
+        <Select
+          ref={selectRef}
+          className={`rounded-full font-thin border-0 py-1 px-1 pl-3 w-[5.65rem] hover:bg-core-hover focus:ring-0`}
+          onChange={(e) => {
+            setSearchTypeSelected(e.target.value);
+          }}
+          value={searchTypeSelected}
+        >
+          <option value="events">Events</option>
+          <option value="users">Users</option>
+        </Select>
+      </div>
+      {/* // )} */}
+      <button
+        onClick={handleSearchClick}
+        className={`w-fit rounded-full border border-black bg-black transition-all duration-[400ms] text-white flex ${
+          isFocused ? "h-8" : "h-7"
+        }`}
+      >
+        <div className={`font-thin text-white overflow-hidden transition-all ${isFocused ? "w-16" : "w-0"}`}>
+          <p className="pt-[2.5px] pl-2">Search</p>
+        </div>
         <SearchIcon />
       </button>
     </div>
