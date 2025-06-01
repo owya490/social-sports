b5777e0b15f3c275ff207df4bc1244d7161ab588
Brian Wang <brian.wang900@gmail.com>
2025-06-01 23:21:16 +1000

    fix

--- functions/lib/move_inactive_events.py
+++ functions/lib/move_inactive_events.py
@@ -71,12 +71,10 @@ def remove_event_from_upcoming_organiser_events(
 
     # might as well scan upcoming events to see if we missed any
     for upcoming_event_id in upcoming_events[:]: # build a shallow copy to not cause concurrent modification issues
-        if upcoming_event_id == event_id:
-            upcoming_events.remove(event_id)
-            continue
         upcoming_event = db.collection(ACTIVE_PUBLIC).document(upcoming_event_id).get()
         if not upcoming_event.exists:
-            raise ValueError(f"eventId {upcoming_event_id} not found in public active events")
+            upcoming_events.remove(upcoming_event_id)
+            continue
         event_dict = upcoming_event.to_dict()
         event_end_date: Timestamp = event_dict.get("endDate").timestamp_pb()
 
@@ -143,7 +141,7 @@ def get_and_move_private_inactive_events(today: date):
 
 
 @scheduler_fn.on_schedule(
-    schedule="every day 00:00",
+    schedule="every day 00:05",
     region="australia-southeast1",
     timezone=scheduler_fn.Timezone("Australia/Sydney"),
 )
