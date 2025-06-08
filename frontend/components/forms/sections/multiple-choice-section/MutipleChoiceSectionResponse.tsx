f8badb85d66b4229a3daad55adfdea8272bae399
Owen <owen.yang@sowaustralia.com>
2025-06-08 21:10:08 +1000

    one more time

--- frontend/components/forms/sections/multiple-choice-section/MutipleChoiceSectionResponse.tsx
+++ frontend/components/forms/sections/multiple-choice-section/MutipleChoiceSectionResponse.tsx
@@ -2,6 +2,7 @@ import { MultipleChoiceSection } from "@/interfaces/FormTypes";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const MutipleChoiceSectionResponse = ({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   multipleChoiceSection,
 }: {
   multipleChoiceSection: MultipleChoiceSection;
