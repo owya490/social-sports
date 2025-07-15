6dd806033db710e238fa1ae4beec18691fe1f156
Owen <owen.yang@sowaustralia.com>
2025-07-15 17:20:50 +1000

    add ignore non sportshub events

--- functions/lib/stripe/webhooks.py
+++ functions/lib/stripe/webhooks.py
@@ -300,6 +300,11 @@ def stripe_webhook_checkout_fulfilment(req: https_fn.Request) -> https_fn.Respon
       logger.info(f"Ignoring event. event={event}")
       return https_fn.Response(status=200)
 
+  SPORTSHUB_URL = "www.sportshub.net.au"
+  if not SPORTSHUB_URL in event["id"]["object"]["cancel_url"] and not SPORTSHUB_URL in event["id"]["object"]["success_url"]:
+    logger.info(f"Ignoring event as it is not a SPORTSHUB event. event={event} success_url={event['id']['object']['success_url']} cancel_url={event['id']['object']['cancel_url']}")
+    return https_fn.Response(status=200)
+
   match event["type"]:
     # Handle the checkout.session.completed event
     case "checkout.session.completed":
