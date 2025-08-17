74b0e21ea9041b6b2787780cb2e667c1bba80ff4
Brian Wang <brian.wang900@gmail.com>
2025-08-17 16:24:42 +1000

    fix hover tooltip for events on organiser navbar

--- frontend/components/organiser/OrganiserNavbar.tsx
+++ frontend/components/organiser/OrganiserNavbar.tsx
@@ -25,18 +25,23 @@ interface OrganiserNavbarProps {
 interface ResponsiveTooltipProps {
   content: string;
   children: ReactNode;
+  disabled?: boolean;
 }
 
-const ResponsiveTooltip = ({ content, children }: ResponsiveTooltipProps) => {
+const ResponsiveTooltip = ({ content, children, disabled = false }: ResponsiveTooltipProps) => {
+  if (disabled) {
+    return <>{children}</>;
+  }
+
   return (
     <div className="relative">
       <div className="hidden sm:block">
-        <Tooltip content={content} placement="right" className="absolute left-full ml-2">
+        <Tooltip content={content} placement="right" className="absolute left-full ml-2" interactive={false}>
           {children}
         </Tooltip>
       </div>
       <div className="block sm:hidden">
-        <Tooltip content={content} placement="top">
+        <Tooltip content={content} placement="top" interactive={false}>
           {children}
         </Tooltip>
       </div>
@@ -97,74 +102,78 @@ export default function OrganiserNavbar({ currPage }: OrganiserNavbarProps) {
         <NavButton href="/organiser/dashboard/" isActive={currPage === "Dashboard"} tooltipContent="Dashboard">
           <HomeIcon className="w-6 stroke-1 stroke-core-text" />
         </NavButton>
-
-        <ResponsiveTooltip content="Events">
-          <Menu as="div" className="relative inline-block text-left">
-            <div className="flex items-centers">
-              <MenuButton
-                className={`flex justify-center items-center self-center h-10 w-10 sm:h-10 sm:w-10 sm:m-auto rounded-md hover:bg-core-hover transition ease-in-out ${
-                  currPage === "EventDashboard" && "bg-core-hover"
-                }`}
-              >
-                <CalendarIcon className="w-6 stroke-1 stroke-core-text" />
-              </MenuButton>
-            </div>
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <MenuItems className="absolute bottom-16 left-0 md:left-16 md:top-0 mt-1 w-52 h-fit origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <div className="px-1 py-1 ">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/organiser/event/dashboard"
-                        className={`${
-                          active ? "text-core-text bg-core-hover" : "text-core-text"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <StarIcon className="w-6 stroke-1 mr-2" />
-                        Event Dashboard
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/organiser/recurring-events"
-                        className={`${
-                          active ? "text-core-text bg-core-hover" : "text-core-text"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <ArrowPathIcon className="w-6 stroke-1 mr-2" />
-                        Recurring Events
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/organiser/event/custom-links"
-                        className={`${
-                          active ? "text-core-text bg-core-hover" : "text-core-text"
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <LinkIcon className="w-6 stroke-1 mr-2" />
-                        Custom Event Links
-                      </Link>
-                    )}
-                  </Menu.Item>
+        <Menu as="div" className="relative inline-block text-left">
+          {({ open }) => {
+            return (
+              <>
+                <div className="flex items-centers">
+                  <ResponsiveTooltip content="Events" disabled={open}>
+                    <MenuButton
+                      className={`flex justify-center items-center self-center h-10 w-10 sm:h-10 sm:w-10 sm:m-auto rounded-md hover:bg-core-hover transition ease-in-out ${
+                        currPage === "EventDashboard" && "bg-core-hover"
+                      }`}
+                    >
+                      <CalendarIcon className="w-6 stroke-1 stroke-core-text" />
+                    </MenuButton>
+                  </ResponsiveTooltip>
                 </div>
-              </MenuItems>
-            </Transition>
-          </Menu>
-        </ResponsiveTooltip>
-
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <MenuItems className="absolute bottom-16 left-0 md:left-16 md:top-0 mt-1 w-52 h-fit origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="px-1 py-1 ">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="/organiser/event/dashboard"
+                            className={`${
+                              active ? "text-core-text bg-core-hover" : "text-core-text"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            <StarIcon className="w-6 stroke-1 mr-2" />
+                            Event Dashboard
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="/organiser/recurring-events"
+                            className={`${
+                              active ? "text-core-text bg-core-hover" : "text-core-text"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            <ArrowPathIcon className="w-6 stroke-1 mr-2" />
+                            Recurring Events
+                          </Link>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Link
+                            href="/organiser/event/custom-links"
+                            className={`${
+                              active ? "text-core-text bg-core-hover" : "text-core-text"
+                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          >
+                            <LinkIcon className="w-6 stroke-1 mr-2" />
+                            Custom Event Links
+                          </Link>
+                        )}
+                      </Menu.Item>
+                    </div>
+                  </MenuItems>
+                </Transition>
+              </>
+            );
+          }}
+        </Menu>{" "}
         <NavButton
           href={`/organiser/event/${eventId}`}
           isActive={currPage === "EventDrilldown"}
@@ -172,19 +181,15 @@ export default function OrganiserNavbar({ currPage }: OrganiserNavbarProps) {
         >
           <BookmarkSquareIcon className="w-6 stroke-1 stroke-core-text" />
         </NavButton>
-
         <NavButton href={`/organiser/forms/gallery`} isActive={currPage === "FormsGallery"} tooltipContent="Forms">
           <PencilSquareIcon className="w-6 stroke-1 stroke-core-text" />
         </NavButton>
-
         <NavButton href="/organiser/metrics" isActive={currPage === "Metrics"} tooltipContent="Metrics">
           <ChartBarIcon className="w-6 stroke-1 stroke-core-text" />
         </NavButton>
-
         <NavButton href="/organiser/gallery" isActive={currPage === "Gallery"} tooltipContent="Gallery">
           <CameraIcon className="w-6 stroke-1 stroke-core-text" />
         </NavButton>
-
         <NavButton href="/organiser/settings" isActive={currPage === "Settings"} tooltipContent="Settings">
           <UserIcon className="w-6 stroke-1 stroke-core-text" />
         </NavButton>
