58ed73f8b0a41a81efedbb30071518c7f4de6eb8
unknown <aidanyschee@gmail.com>
2025-09-20 12:18:03 +1000

    update header

--- frontend/app/(footer)/layout.tsx
+++ frontend/app/(footer)/layout.tsx
@@ -1,5 +1,10 @@
 import Footer from "@/components/Footer";
 
+export const metadata = {
+  title: "SPORTSHUB | Find your next social sport session!", 
+  description: "Sportshub helps you find and book local sports events.",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
