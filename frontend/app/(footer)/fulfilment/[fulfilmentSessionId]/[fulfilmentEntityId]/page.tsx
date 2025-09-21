85693cb37808be1178cfe4e1fac398977846c9b9
Brian Wang <brian.wang900@gmail.com>
2025-09-21 20:34:07 +1000

    fix linting

--- frontend/app/(footer)/fulfilment/[fulfilmentSessionId]/[fulfilmentEntityId]/page.tsx
+++ frontend/app/(footer)/fulfilment/[fulfilmentSessionId]/[fulfilmentEntityId]/page.tsx
@@ -330,7 +330,7 @@ function EndFulfilmentHandler({
 
   useEffect(() => {
     let cancelled = false;
-    (async () => {
+    (() => {
       try {
         if (cancelled) return;
         logger.info(
