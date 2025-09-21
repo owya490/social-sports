dc6038d20620dbc504bb3e2c4c0eec505e23f68d
Owen <owen.yang@sowaustralia.com>
2025-09-21 17:43:15 +1000

    ensure that cancel and success urls are lower case

--- functions/lib/stripe/webhooks.py
+++ functions/lib/stripe/webhooks.py
@@ -427,8 +427,8 @@ def stripe_webhook_checkout_fulfilment(req: https_fn.Request) -> https_fn.Respon
     # If we are in prod, we only want to process events from SPORTSHUB_URL
     if (
         IS_PROD
-        and SPORTSHUB_URL not in event["data"]["object"]["cancel_url"]
-        and SPORTSHUB_URL not in event["data"]["object"]["success_url"]
+        and SPORTSHUB_URL not in str(event["data"]["object"]["cancel_url"]).lower()
+        and SPORTSHUB_URL not in str(event["data"]["object"]["success_url"]).lower()
     ):
         logger.info(
             f"Ignoring event as it is not a SPORTSHUB event. event={event} success_url={event['data']['object']['success_url']} cancel_url={event['data']['object']['cancel_url']}"
