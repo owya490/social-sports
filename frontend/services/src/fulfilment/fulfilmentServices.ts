f4fd709d9429b1154c52c109a7df0ba8475bfa2d
Owen <owen.yang@sowaustralia.com>
2025-09-23 00:43:01 +1000

    enable fulfilment for sportshub test event

--- frontend/services/src/fulfilment/fulfilmentServices.ts
+++ frontend/services/src/fulfilment/fulfilmentServices.ts
@@ -33,7 +33,7 @@ const fulfilmentSessionEnabledUserIdList = [
   "c5vFAZ3NlSXVuHGrwlkCjJr3RXX2", // owen dev
 ];
 const fulfilmentSessionEnabledEventIdList: string[] = [
-  "brian was here"
+  "yr1bmHx9mpss5jAXPhEE", // sportshub test event
   // "0kcqoQMnRE9OV3ezstZt", // syrio jersey
 ];
 
