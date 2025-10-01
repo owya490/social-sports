8b94a1b0eb659a740646df065f7c3699ca56794d
Brian Wang <brian.wang900@gmail.com>
2025-10-01 22:43:15 +1000

    initial recurrence cron transaction

--- functions/lib/functions/src/main/java/com/functions/events/utils/EventsUtils.java
+++ functions/lib/functions/src/main/java/com/functions/events/utils/EventsUtils.java
@@ -3,7 +3,7 @@ package com.functions.events.utils;
 import com.functions.users.models.PrivateUserData;
 import com.functions.users.models.PublicUserData;
 import com.functions.users.services.Users;
-
+import com.google.cloud.firestore.Transaction;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,26 +14,26 @@ public class EventsUtils {
                 .collect(Collectors.toList());
     }
 
-    public static void addEventIdToUserOrganiserPublicUpcomingEvents(String userId, String eventId)
+    public static void addEventIdToUserOrganiserPublicUpcomingEvents(String userId, String eventId, Transaction transaction)
             throws Exception {
-        PublicUserData publicUserData = Users.getPublicUserDataById(userId);
+        PublicUserData publicUserData = Users.getPublicUserDataById(userId, transaction);
 
         List<String> publicUpcomingOrganiserEvents =
                 publicUserData.getPublicUpcomingOrganiserEvents();
         publicUpcomingOrganiserEvents.add(eventId);
         publicUserData.setPublicUpcomingOrganiserEvents(publicUpcomingOrganiserEvents);
 
-        Users.updatePublicUserData(userId, publicUserData);
+        Users.updatePublicUserData(userId, publicUserData, transaction);
     }
 
-    public static void addEventIdToUserOrganiserEvents(String userId, String eventId)
+    public static void addEventIdToUserOrganiserEvents(String userId, String eventId, Transaction transaction)
             throws Exception {
-        PrivateUserData privateUserData = Users.getPrivateUserDataById(userId);
+        PrivateUserData privateUserData = Users.getPrivateUserDataById(userId, transaction);
 
         List<String> organiserEvents = privateUserData.getOrganiserEvents();
         organiserEvents.add(eventId);
         privateUserData.setOrganiserEvents(organiserEvents);
 
-        Users.updatePrivateUserData(userId, privateUserData);
+        Users.updatePrivateUserData(userId, privateUserData, transaction);
     }
 }
