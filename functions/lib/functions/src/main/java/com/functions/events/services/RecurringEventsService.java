bc202587351e79a4a9a6e3cc5f3477814d96f7ba
Brian Wang <brian.wang900@gmail.com>
2025-10-10 22:41:37 +1100

    check for recurring events

--- functions/lib/functions/src/main/java/com/functions/events/services/RecurringEventsService.java
+++ functions/lib/functions/src/main/java/com/functions/events/services/RecurringEventsService.java
@@ -1,5 +1,17 @@
 package com.functions.events.services;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.functions.events.models.NewEventData;
 import com.functions.events.models.NewRecurrenceData;
 import com.functions.events.models.RecurrenceData;
@@ -10,17 +22,6 @@ import com.functions.users.services.Users;
 import com.functions.utils.TimeUtils;
 import com.google.cloud.Timestamp;
 import com.google.common.annotations.VisibleForTesting;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class RecurringEventsService {
     private static final Logger logger = LoggerFactory.getLogger(RecurringEventsService.class);
@@ -88,11 +89,13 @@ public class RecurringEventsService {
             boolean isRecurrenceActive = recurrenceTemplate.getEventData().getIsActive();
             if (!isRecurrenceActive) {
                 List<Timestamp> allRecurrences = recurrenceTemplate.getRecurrenceData().getAllRecurrences();
-                Timestamp lastRecurrence = allRecurrences.get(allRecurrences.size() - 1);
-                // If the lastRecurrence is in the future, we have re-enabled the Recurring Events
-                if (lastRecurrence.getSeconds() > Timestamp.now().getSeconds()) {
-                    RecurrenceTemplateRepository.moveRecurrenceTemplateToActive(recurrenceTemplateId, recurrenceTemplate);
-                    logger.info("Successfully moved Recurrence Template {} to active", recurrenceTemplateId);
+                if (!allRecurrences.isEmpty()) {
+                    Timestamp lastRecurrence = allRecurrences.get(allRecurrences.size() - 1);
+                    // If the lastRecurrence is in the future, we have re-enabled the Recurring Events
+                    if (lastRecurrence.getSeconds() > Timestamp.now().getSeconds()) {
+                        RecurrenceTemplateRepository.moveRecurrenceTemplateToActive(recurrenceTemplateId, recurrenceTemplate);
+                        logger.info("Successfully moved Recurrence Template {} to active", recurrenceTemplateId);
+                    }
                 }
             }
 
