480d11fd94e39581651e8ce726757d7538f59afd
Owen <owen.yang@sowaustralia.com>
2025-10-25 03:40:43 +1100

    phase out date picker

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,7 +8,6 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   // experimental: {
   //   optimizeUniversalDefaults: true,
