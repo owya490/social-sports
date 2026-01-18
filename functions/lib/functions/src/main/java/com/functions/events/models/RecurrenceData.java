7a4a2dc8d09221b5c651943cc548e5c228028721
Daniel Zhang <daniel@Daniels-MacBook-Pro.local>
2026-01-19 02:56:25 +1100

    reserved attendess

--- functions/lib/functions/src/main/java/com/functions/events/models/RecurrenceData.java
+++ functions/lib/functions/src/main/java/com/functions/events/models/RecurrenceData.java
@@ -1,8 +1,10 @@
 package com.functions.events.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.google.cloud.Timestamp;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,6 +12,7 @@ import java.util.Map;
 @Builder(toBuilder=true)
 @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RecurrenceData {
 	RecurrenceData.Frequency frequency;
 	Integer recurrenceAmount;
@@ -22,21 +25,30 @@ public class RecurrenceData {
 	 */
 	Map<String, String> pastRecurrences;
 
+	/**
+	 * List of reserved slots for specific email addresses.
+	 * These emails will have spots automatically reserved in each recurring event.
+	 */
+	@Builder.Default
+	List<ReservedSlot> reservedSlots = new ArrayList<>();
+
 	public static RecurrenceData.RecurrenceDataBuilder builderFromNewRecurrenceData(NewRecurrenceData newRecurrenceData) {
 		return RecurrenceData.builder()
 				.frequency(newRecurrenceData.getFrequency())
 				.recurrenceAmount(newRecurrenceData.getRecurrenceAmount())
 				.createDaysBefore(newRecurrenceData.getCreateDaysBefore())
-				.recurrenceEnabled(newRecurrenceData.getRecurrenceEnabled());
+				.recurrenceEnabled(newRecurrenceData.getRecurrenceEnabled())
+				.reservedSlots(newRecurrenceData.getReservedSlots() != null ? newRecurrenceData.getReservedSlots() : new ArrayList<>());
 	}
 
 	public NewRecurrenceData extractNewRecurrenceData() {
-		return NewRecurrenceData.builder()
-				.frequency(this.frequency)
-				.recurrenceAmount(this.recurrenceAmount)
-				.createDaysBefore(this.createDaysBefore)
-				.recurrenceEnabled(this.recurrenceEnabled)
-				.build();
+		NewRecurrenceData data = new NewRecurrenceData();
+		data.setFrequency(this.frequency);
+		data.setRecurrenceAmount(this.recurrenceAmount);
+		data.setCreateDaysBefore(this.createDaysBefore);
+		data.setRecurrenceEnabled(this.recurrenceEnabled);
+		data.setReservedSlots(this.reservedSlots != null ? this.reservedSlots : new ArrayList<>());
+		return data;
 	}
 
 	@Getter
