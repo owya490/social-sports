5e9a12813689e801b820461d5792c72673691083
Brian Wang <brian.wang900@gmail.com>
2026-09-05 08:56:11 +1000

    Remove unused legacy event link helpers

--- functions/lib/functions/src/main/java/com/functions/events/services/CustomEventLinksService.java
+++ functions/lib/functions/src/main/java/com/functions/events/services/CustomEventLinksService.java
@@ -14,27 +14,6 @@ import com.google.cloud.firestore.Transaction;
 public class CustomEventLinksService {
   private static final Logger logger = LoggerFactory.getLogger(CustomEventLinksService.class);
 
-  public static List<String> updateEventLinksPointedToRecurrence(String userId, String recurrenceTemplateId, String eventId) {
-    logger.info("Updating event links pointed to recurrence template {}", recurrenceTemplateId);
-    List<CustomEventLink> allReferencedEventLinks = CustomEventLinksRepository.getAllEventLinksPointedToRecurrence(userId, recurrenceTemplateId);
-    logger.info("All referenced event links: {}", allReferencedEventLinks);
-    allReferencedEventLinks.forEach(eventLink -> {
-      CustomEventLink updatedEventLink = new CustomEventLink(
-        eventLink.getCustomEventLink(),
-        eventLink.getCustomEventLinkName(),
-        eventLink.getId(),
-        eventId,
-        eventLink.getReferenceId(),
-        eventLink.getReferenceName(),
-        eventLink.getType()
-      );
-      CustomEventLinksRepository.saveCustomEventLink(userId, updatedEventLink);
-    });
-    List<String> updatedEventLinks = allReferencedEventLinks.stream().map(CustomEventLink::getCustomEventLink).collect(Collectors.toList());
-    logger.info("Updated {} event links: {}", updatedEventLinks.size(), updatedEventLinks);
-    return updatedEventLinks;
-  }
-
   public static List<DocumentSnapshot> getEventLinkDocumentsPointedToRecurrence(String userId,
       String recurrenceTemplateId, Transaction transaction) throws Exception {
     return CustomEventLinksRepository.getEventLinkDocumentsPointedToRecurrence(
